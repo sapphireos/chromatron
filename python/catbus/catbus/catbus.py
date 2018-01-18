3b2472bcf7a25ae560db5c1ed4467e955dc3b30b
JeremyB <jeremybillheimer@gmail.com>
2018-01-18 14:38:33 -0600

    license update

--- python/catbus/catbus/catbus.py
+++ python/catbus/catbus/catbus.py
@@ -2,8 +2,8 @@
 # 
 #     This file is part of the Sapphire Operating System.
 # 
-#     Copyright (C) 2013-2017  Jeremy Billheimer
-#     
+#     Copyright (C) 2013-2018  Jeremy Billheimer
+# 
 # 
 #     This program is free software: you can redistribute it and/or modify
 #     it under the terms of the GNU General Public License as published by
@@ -17,6 +17,7 @@
 # 
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
 # </license>
 #
 
