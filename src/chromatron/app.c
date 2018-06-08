4fa0939759c058ee23ecb39e7106eb0f14c8d2ee
JeremyB <jeremybillheimer@gmail.com>
2018-06-08 10:25:54 -0500

    clean up

--- src/chromatron/app.c
+++ src/chromatron/app.c
@@ -117,20 +117,5 @@ void app_v_init( void ){
     #endif
 
     svr_v_init();
-
-    // catbus_query_t query;
-    // memset( &query, 0, sizeof(query) );
-
-    // query.tags[0] = __KV__stuff;
-
-    // catbus_l_send( __KV__kv_test_key, __KV__kv_test_key, &query );
-    
-
-    // kvdb_i8_add( __KV__test_meow, CATBUS_TYPE_INT16, 1, 0, 0 );
-    // kvdb_v_set_name_P( PSTR("test_meow") );
-
-    // query.tags[0] = __KV__stuff;
-
-    // catbus_l_recv( __KV__test_meow, __KV__kv_test_key, &query );
 }
 
