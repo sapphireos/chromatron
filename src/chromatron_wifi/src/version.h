0603f235ec461b2402d99e6eec91b8194be88f4e
JeremyB <jeremybillheimer@gmail.com>
2018-08-16 15:38:14 -0500

    version bumps

--- src/chromatron_wifi/src/version.h
+++ src/chromatron_wifi/src/version.h
@@ -23,9 +23,9 @@
 #ifndef _VERSION_H
 #define _VERSION_H
 
-#define VERSION_MAJOR   1
+#define VERSION_MAJOR   2
 #define VERSION_MINOR   0
-#define VERSION_PATCH   2
+#define VERSION_PATCH   0
 
 
 #endif
