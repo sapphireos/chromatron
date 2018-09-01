b2e9c37418049320d7be46f68f87b8d7c5c0d66e
JeremyB <jeremybillheimer@gmail.com>
2018-08-31 19:46:35 -0500

    hal io driver

--- src/hal/stm32f7/hal_io.c
+++ src/hal/stm32f7/hal_io.c
@@ -27,7 +27,6 @@
 
 #include "system.h"
 #include "target.h"
-#include "os_irq.h"
 
 #include "hal_io.h"
 
@@ -35,10 +34,13 @@
 
 
 
-
 void io_v_init( void ){
 
-    
+    // set all other pins to inputs pulled down
+    for( uint8_t i = 0; i < IO_PIN_COUNT; i++ ){
+
+        io_v_set_mode( i, IO_MODE_INPUT_PULLDOWN );
+    }
 }
 
 uint8_t io_u8_get_board_rev( void ){
@@ -49,24 +51,248 @@ uint8_t io_u8_get_board_rev( void ){
 
 void io_v_set_mode( uint8_t pin, io_mode_t8 mode ){
 
+	LL_GPIO_InitTypeDef GPIO_InitStruct;
+	GPIO_TypeDef *GPIOx;
+    GPIO_InitStruct.Mode        = LL_GPIO_MODE_INPUT;
+    GPIO_InitStruct.Speed       = LL_GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.OutputType  = LL_GPIO_OUTPUT_OPENDRAIN;
+    GPIO_InitStruct.Pull        = LL_GPIO_PULL_NO;
+
+	switch( pin ){
+        case IO_PIN_0:
+        	GPIOx = IO_PIN0_PORT;
+        	GPIO_InitStruct.Pin = IO_PIN0_PIN;
+            break;
+
+        case IO_PIN_1:
+        	GPIOx = IO_PIN1_PORT;
+        	GPIO_InitStruct.Pin = IO_PIN1_PIN;
+            break;
+
+        case IO_PIN_2:
+        	GPIOx = IO_PIN2_PORT;
+        	GPIO_InitStruct.Pin = IO_PIN2_PIN;
+            break;
+
+        case IO_PIN_3:
+        	GPIOx = IO_PIN3_PORT;
+        	GPIO_InitStruct.Pin = IO_PIN3_PIN;
+            break;
+
+        case IO_PIN_4:
+        	GPIOx = IO_PIN4_PORT;
+        	GPIO_InitStruct.Pin = IO_PIN4_PIN;
+            break;
+
+        case IO_PIN_5:
+        	GPIOx = IO_PIN5_PORT;
+        	GPIO_InitStruct.Pin = IO_PIN5_PIN;
+            break;
+
+        default:
+            return;
+            break;
+    }
+
+
+    if( mode >= IO_MODE_OUTPUT ){
+
+    	GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+
+        if( mode == IO_MODE_OUTPUT_OPEN_DRAIN ){
+
+        	GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_OPENDRAIN;
+        }
+        else{
+
+        	GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+        }
+    }
+    else{
+
+    	GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+
+        if( mode == IO_MODE_INPUT_PULLUP ){
+
+        	GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+        }
+        else if( mode == IO_MODE_INPUT_PULLDOWN ){
+
+        	GPIO_InitStruct.Pull = LL_GPIO_PULL_DOWN;
+        }
+    }
+
+    LL_GPIO_Init( GPIOx, &GPIO_InitStruct );
 }
 
 
 io_mode_t8 io_u8_get_mode( uint8_t pin ){
 
+	GPIO_TypeDef *GPIOx;
+	uint32_t gpio_pin;
+
+	switch( pin ){
+        case IO_PIN_0:
+        	GPIOx = IO_PIN0_PORT;
+        	gpio_pin = IO_PIN0_PIN;
+            break;
+
+        case IO_PIN_1:
+        	GPIOx = IO_PIN1_PORT;
+        	gpio_pin = IO_PIN1_PIN;
+            break;
+
+        case IO_PIN_2:
+        	GPIOx = IO_PIN2_PORT;
+        	gpio_pin = IO_PIN2_PIN;
+            break;
+
+        case IO_PIN_3:
+        	GPIOx = IO_PIN3_PORT;
+        	gpio_pin = IO_PIN3_PIN;
+            break;
+
+        case IO_PIN_4:
+        	GPIOx = IO_PIN4_PORT;
+        	gpio_pin = IO_PIN4_PIN;
+            break;
+
+        case IO_PIN_5:
+        	GPIOx = IO_PIN5_PORT;
+        	gpio_pin = IO_PIN5_PIN;
+            break;
+
+        default:
+            return IO_MODE_INPUT;
+            break;
+    }
 
 
-    return 0;
+    if( LL_GPIO_GetPinMode( GPIOx, gpio_pin ) == LL_GPIO_MODE_OUTPUT ){
+
+    	if( LL_GPIO_GetPinOutputType( GPIOx, gpio_pin ) == LL_GPIO_OUTPUT_PUSHPULL ){
+
+    		return IO_MODE_OUTPUT;
+    	}
+    	else{
+
+    		return IO_MODE_OUTPUT_OPEN_DRAIN;
+    	}
+    }
+    else{
+
+    	if( LL_GPIO_GetPinPull( GPIOx, gpio_pin ) == LL_GPIO_PULL_UP ){
+
+    		return IO_MODE_INPUT_PULLUP;
+    	}
+    	else if( LL_GPIO_GetPinPull( GPIOx, gpio_pin ) == LL_GPIO_PULL_DOWN ){
+
+    		return IO_MODE_INPUT_PULLDOWN;
+    	}
+    	else{
+
+    		return IO_MODE_INPUT;	
+    	}
+    }
+
+    return IO_MODE_INPUT;
 }
 
 void io_v_digital_write( uint8_t pin, bool state ){
 
+	GPIO_TypeDef *GPIOx;
+	uint32_t gpio_pin;
 
+	switch( pin ){
+        case IO_PIN_0:
+        	GPIOx = IO_PIN0_PORT;
+        	gpio_pin = IO_PIN0_PIN;
+            break;
+
+        case IO_PIN_1:
+        	GPIOx = IO_PIN1_PORT;
+        	gpio_pin = IO_PIN1_PIN;
+            break;
+
+        case IO_PIN_2:
+        	GPIOx = IO_PIN2_PORT;
+        	gpio_pin = IO_PIN2_PIN;
+            break;
+
+        case IO_PIN_3:
+        	GPIOx = IO_PIN3_PORT;
+        	gpio_pin = IO_PIN3_PIN;
+            break;
+
+        case IO_PIN_4:
+        	GPIOx = IO_PIN4_PORT;
+        	gpio_pin = IO_PIN4_PIN;
+            break;
+
+        case IO_PIN_5:
+        	GPIOx = IO_PIN5_PORT;
+        	gpio_pin = IO_PIN5_PIN;
+            break;
+
+        default:
+            return;
+            break;
+    }
+
+    if( state ){
+
+    	LL_GPIO_SetOutputPin( GPIOx, gpio_pin );
+    }
+    else{
+
+    	LL_GPIO_ResetOutputPin( GPIOx, gpio_pin );
+    }
 }
 
 bool io_b_digital_read( uint8_t pin ){
 
-    return 0;
+	GPIO_TypeDef *GPIOx;
+	uint32_t gpio_pin;
+
+	switch( pin ){
+        case IO_PIN_0:
+        	GPIOx = IO_PIN0_PORT;
+        	gpio_pin = IO_PIN0_PIN;
+            break;
+
+        case IO_PIN_1:
+        	GPIOx = IO_PIN1_PORT;
+        	gpio_pin = IO_PIN1_PIN;
+            break;
+
+        case IO_PIN_2:
+        	GPIOx = IO_PIN2_PORT;
+        	gpio_pin = IO_PIN2_PIN;
+            break;
+
+        case IO_PIN_3:
+        	GPIOx = IO_PIN3_PORT;
+        	gpio_pin = IO_PIN3_PIN;
+            break;
+
+        case IO_PIN_4:
+        	GPIOx = IO_PIN4_PORT;
+        	gpio_pin = IO_PIN4_PIN;
+            break;
+
+        case IO_PIN_5:
+        	GPIOx = IO_PIN5_PORT;
+        	gpio_pin = IO_PIN5_PIN;
+            break;
+
+        default:
+            return FALSE;
+            break;
+    }
+
+    uint32_t port = LL_GPIO_ReadInputPort( GPIOx );
+
+    return ( port & ( 1 << gpio_pin ) ) != 0;
 }
 
 bool io_b_button_down( void ){
@@ -83,8 +309,6 @@ void io_v_enable_interrupt(
     io_int_handler_t handler,
     io_int_mode_t8 mode )
 {
-
-   
 
 }
 
