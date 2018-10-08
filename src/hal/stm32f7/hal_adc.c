5198e5411f2cdc9f9ec130842368f80b797ddee8
JeremyB <jeremybillheimer@gmail.com>
2018-10-08 14:18:42 -0500

    some adc init

--- src/hal/stm32f7/hal_adc.c
+++ src/hal/stm32f7/hal_adc.c
@@ -25,17 +25,62 @@
 
 #include "system.h"
 #include "adc.h"
+#include "hal_io.h"
 #include "hal_adc.h"
 
-
+static ADC_HandleTypeDef hadc1;
 
 void adc_v_init( void ){
 
+	__HAL_RCC_ADC1_CLK_ENABLE();
+  
+    /**ADC1 GPIO Configuration    
+    PC0     ------> ADC1_IN10 
+    */
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitStruct.Pin = VMON_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(VMON_GPIO_Port, &GPIO_InitStruct);
+
+
+	ADC_ChannelConfTypeDef sConfig;
+
+	/**Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
+	*/
+	hadc1.Instance = ADC1;
+	hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+	hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+	hadc1.Init.ScanConvMode = DISABLE;
+	hadc1.Init.ContinuousConvMode = DISABLE;
+	hadc1.Init.DiscontinuousConvMode = DISABLE;
+	hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+	hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+	hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+	hadc1.Init.NbrOfConversion = 1;
+	hadc1.Init.DMAContinuousRequests = DISABLE;
+	hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+	if (HAL_ADC_Init(&hadc1) != HAL_OK)
+	{
+		_Error_Handler(__FILE__, __LINE__);
+	}
+
+	/**Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+	*/
+	sConfig.Channel = ADC_CHANNEL_10;
+	sConfig.Rank = ADC_REGULAR_RANK_1;
+	sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+	if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+	{
+		_Error_Handler(__FILE__, __LINE__);
+	}
 }
 
 void adc_v_shutdown( void ){
 
-
+	__HAL_RCC_ADC1_CLK_DISABLE();
+	
+	HAL_GPIO_DeInit(VMON_GPIO_Port, VMON_Pin);
 }
 
 void adc_v_set_ref( uint8_t ref ){
