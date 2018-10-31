e97b4f6768d4ea972e55a9cb22d71c18d1dc7cb8
JeremyB <jeremybillheimer@gmail.com>
2018-10-30 22:01:34 -0500

    fixing stuffc

--- src/hal/stm32f7/target.h
+++ src/hal/stm32f7/target.h
@@ -76,7 +76,7 @@
 
 #define FLASH_START 	0x08000000
 // #define FW_INFO_ADDRESS 0x00008200 // this must match the offset in the makefile!
-#define FW_INFO_ADDRESS 0x08000200 // this must match the offset in the makefile!
+#define FW_INFO_ADDRESS 0x00000200 // this must match the offset in the makefile!
 #define FW_LENGTH_ADDRESS 0x00000200
 
 #define MAX_PROGRAM_LENGTH ( (uint32_t)( (uint32_t)PAGE_SIZE * (uint32_t)N_APP_PAGES ) )
