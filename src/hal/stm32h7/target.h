677688d3a540d7940b25d01354e09992fbae9c75
JeremyB <jeremybillheimer@gmail.com>
2018-12-12 21:32:58 -0600

    gcc ignoring section-start.  fun.

--- src/hal/stm32h7/target.h
+++ src/hal/stm32h7/target.h
@@ -97,7 +97,7 @@
 	#define FLASH_START 	( 0x08000000 + 32768 )
 #endif
 
-#define FW_INFO_ADDRESS 0x00000200 // this must match the offset in the makefile!
+#define FW_INFO_ADDRESS 0x00000400 // this must match the offset in the makefile!
 #define FW_LENGTH_ADDRESS FW_INFO_ADDRESS
 
 
