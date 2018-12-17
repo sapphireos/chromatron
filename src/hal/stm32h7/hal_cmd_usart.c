a922fdc2a1cdc1161730c5ac44cfab1532c98061
JeremyB <jeremybillheimer@gmail.com>
2018-12-16 20:35:48 -0600

    cmd usart init

--- src/hal/stm32h7/hal_cmd_usart.c
+++ src/hal/stm32h7/hal_cmd_usart.c
@@ -139,13 +139,19 @@ void cmd_usart_v_init( void ){
     // init IO pins
     GPIO_InitTypeDef GPIO_InitStruct;
 
-    GPIO_InitStruct.Pin = CMD_USART_TX_Pin|CMD_USART_RX_Pin;
+    GPIO_InitStruct.Pin = CMD_USART_RX_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART3;
+    HAL_GPIO_Init(CMD_USART_RX_GPIO_Port, &GPIO_InitStruct);
+
+    GPIO_InitStruct.Pin = CMD_USART_TX_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF7_USART3;
     HAL_GPIO_Init(CMD_USART_TX_GPIO_Port, &GPIO_InitStruct);
-
 
     // initialize command usart
     huart3.Instance = HAL_CMD_USART;
