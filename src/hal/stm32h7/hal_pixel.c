615c7114442e017e0d9d16861462dd1fa8a006d5
JeremyB <jeremybillheimer@gmail.com>
2018-12-29 12:48:27 -0600

    debug

--- src/hal/stm32h7/hal_pixel.c
+++ src/hal/stm32h7/hal_pixel.c
@@ -216,7 +216,8 @@ void hal_pixel_v_start_transfer( uint8_t driver, uint8_t *data, uint16_t len ){
     }
     else if( driver == 3 ){
 
-        HAL_SPI_Transmit_DMA( &pix_spi3, data, len );   
+        // HAL_SPI_Transmit_DMA( &pix_spi3, data, len );   
+        HAL_SPI_Transmit( &pix_spi3, data, len, 500 );   
     }
     else if( driver == 4 ){
 
