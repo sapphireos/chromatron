5394d2a554711380ed41fc4fd644fdd5e29659a1
JeremyB <jeremybillheimer@gmail.com>
2018-12-31 11:52:31 -0600

    update requests to 2.20.0

--- python/sapphire/setup.py
+++ python/sapphire/setup.py
@@ -47,7 +47,7 @@ setup(
     install_requires=[
         "catbus",
         "appdirs==1.4.3",
-        "requests==2.18.4",
+        "requests==2.20.0",
         "setuptools == 34.2.0",
         "pyserial == 3.2.1",
         "bitstring == 3.1.5",
