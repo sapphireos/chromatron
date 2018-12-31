91224cacf24da6eebf147a6be168161a37711685
JeremyB <jeremybillheimer@gmail.com>
2018-12-31 11:42:33 -0600

    check if debugger attached when doing a trace print

--- src/hal/stm32h7/Drivers/CMSIS/diag/trace_impl.c
+++ src/hal/stm32h7/Drivers/CMSIS/diag/trace_impl.c
@@ -234,33 +234,39 @@ _trace_write_semihosting_stdout (const char* buf, size_t nbyte)
 static ssize_t
 _trace_write_semihosting_debug (const char* buf, size_t nbyte)
 {
-  // Since the single character debug channel is quite slow, try to
-  // optimise and send a null terminated string, if possible.
-  if (buf[nbyte] == '\0')
-    {
-      // send string
-      call_host (SEMIHOSTING_SYS_WRITE0, (void*) buf);
-    }
-  else
-    {
-      // If not, use a local buffer to speed things up
-      char tmp[OS_INTEGER_TRACE_TMP_ARRAY_SIZE];
-      size_t togo = nbyte;
-      while (togo > 0)
-        {
-          unsigned int n = ((togo < sizeof(tmp)) ? togo : sizeof(tmp));
-          unsigned int i = 0;
-          for (; i < n; ++i, ++buf)
-            {
-              tmp[i] = *buf;
-            }
-          tmp[i] = '\0';
 
-          call_host (SEMIHOSTING_SYS_WRITE0, (void*) tmp);
+  if (CoreDebug->DHCSR & CoreDebug_DHCSR_C_DEBUGEN_Msk)
+  {
+    // Debugger is connected
+    
+    // Since the single character debug channel is quite slow, try to
+    // optimise and send a null terminated string, if possible.
+    if (buf[nbyte] == '\0')
+      {
+        // send string
+        call_host (SEMIHOSTING_SYS_WRITE0, (void*) buf);
+      }
+    else
+      {
+        // If not, use a local buffer to speed things up
+        char tmp[OS_INTEGER_TRACE_TMP_ARRAY_SIZE];
+        size_t togo = nbyte;
+        while (togo > 0)
+          {
+            unsigned int n = ((togo < sizeof(tmp)) ? togo : sizeof(tmp));
+            unsigned int i = 0;
+            for (; i < n; ++i, ++buf)
+              {
+                tmp[i] = *buf;
+              }
+            tmp[i] = '\0';
 
-          togo -= n;
-        }
-    }
+            call_host (SEMIHOSTING_SYS_WRITE0, (void*) tmp);
+
+            togo -= n;
+          }
+      }
+  }
 
   // All bytes written
   return (ssize_t) nbyte;
