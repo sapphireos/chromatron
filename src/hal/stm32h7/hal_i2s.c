2763ddeb17f21761c297370ad6d312f4abc92c95
JeremyB <jeremybillheimer@gmail.com>
2019-02-21 00:30:32 -0600

    wtf

--- src/hal/stm32h7/hal_i2s.c
+++ src/hal/stm32h7/hal_i2s.c
@@ -54,7 +54,7 @@ static const i2s_port_def_t i2s_port_def = {
 	GPIO_AF5_SPI1,
 };
 
-static uint16_t i2s_buffer[1024];
+static uint32_t i2s_buffer[128];
 
 static I2S_HandleTypeDef i2s_handle;
 static DMA_HandleTypeDef i2s_dma;
@@ -142,8 +142,8 @@ void hal_i2s_v_start( uint16_t sample_rate, uint8_t sample_bits, bool stereo ){
 
     __HAL_LINKDMA( &i2s_handle, hdmarx, i2s_dma );
 
-    // HAL_I2S_Receive_DMA( &i2s_handle, i2s_buffer, sizeof(i2s_buffer) );
-    HAL_I2S_Receive( &i2s_handle, i2s_buffer, sizeof(i2s_buffer), 1000 );
+    HAL_I2S_Receive( &i2s_handle, i2s_buffer, cnt_of_array(i2s_buffer), 1000 );
+    HAL_I2S_Receive_DMA( &i2s_handle, i2s_buffer, cnt_of_array(i2s_buffer) );
 }
 
 
