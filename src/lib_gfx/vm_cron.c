06e3c4d747f9a5ea4c788841bb0d80f0d76b2899
JeremyB <jeremybillheimer@gmail.com>
2019-02-21 23:23:15 -0600

    fix indentation

--- src/lib_gfx/vm_cron.c
+++ src/lib_gfx/vm_cron.c
@@ -42,24 +42,24 @@ PT_THREAD( cron_thread( pt_t *pt, void *state ) );
 
 static void calc_deadline( cron_job_t *cron ){
 
-	
-	// datetime_t datetime_now;
-	// ntp_ts_t ntp_local_now = time_t_local_now();
+    
+    // datetime_t datetime_now;
+    // ntp_ts_t ntp_local_now = time_t_local_now();
 
  //    datetime_v_seconds_to_datetime( ntp_local_now.seconds, &datetime_now );
 
  //    datetime_t datetime_cron;
  //    // memset( &datetime_cron, 0, sizeof(datetime_cron) );
  //    datetime_cron = datetime_now;
-	
-	// if( cron->seconds >= 0 ){
+    
+    // if( cron->seconds >= 0 ){
 
  //        datetime_cron.seconds = cron->seconds;
  //    }
 
  //    if( cron->minutes >= 0 ){
 
- //     	datetime_cron.minutes = cron->minutes;   
+ //         datetime_cron.minutes = cron->minutes;   
  //    }
 
  //    if( cron->hours >= 0 ){
@@ -91,7 +91,7 @@ static void calc_deadline( cron_job_t *cron ){
 
 void vm_cron_v_init( void ){
 
-	list_v_init( &cron_list );
+    list_v_init( &cron_list );
 
     thread_t_create( cron_thread,
              PSTR("cron"),
@@ -101,37 +101,37 @@ void vm_cron_v_init( void ){
 
 void vm_cron_v_load( uint8_t vm_id, vm_state_t *state, file_t f ){
 
-	// make sure this vm's cron jobs are unloaded first
-	vm_cron_v_unload( vm_id );
+    // make sure this vm's cron jobs are unloaded first
+    vm_cron_v_unload( vm_id );
 
-	fs_v_seek( f, sizeof(uint32_t) + state->cron_start );
+    fs_v_seek( f, sizeof(uint32_t) + state->cron_start );
 
-	cron_job_t cron_job;
-	cron_job.vm_id = vm_id;
+    cron_job_t cron_job;
+    cron_job.vm_id = vm_id;
 
-	for( uint8_t i = 0; i < state->cron_count; i++ ){
+    for( uint8_t i = 0; i < state->cron_count; i++ ){
 
-		fs_i16_read( f, (uint8_t *)&cron_job.cron, sizeof(cron_job.cron) );
+        fs_i16_read( f, (uint8_t *)&cron_job.cron, sizeof(cron_job.cron) );
 
-		list_node_t ln = list_ln_create_node2( &cron_job, sizeof(cron_job), MEM_TYPE_CRON_JOB );
+        list_node_t ln = list_ln_create_node2( &cron_job, sizeof(cron_job), MEM_TYPE_CRON_JOB );
 
-		if( ln < 0 ){
+        if( ln < 0 ){
 
-			return;
-		}
+            return;
+        }
 
-		list_v_insert_tail( &cron_list, ln );
-	}
+        list_v_insert_tail( &cron_list, ln );
+    }
 }
 
 void vm_cron_v_unload( uint8_t vm_id ){
 
-  	list_node_t ln = cron_list.head;
-  	list_node_t next_ln;
+    list_node_t ln = cron_list.head;
+    list_node_t next_ln;
 
     while( ln > 0 ){
 
-    	next_ln = list_ln_next( ln );
+        next_ln = list_ln_next( ln );
 
         cron_job_t *entry = list_vp_get_data( ln );
 
@@ -153,10 +153,10 @@ PT_BEGIN( pt );
     
     while(1){
 
-    	// wait for sync
-    	THREAD_WAIT_WHILE( pt, !time_b_is_sync() );
+        // wait for sync
+        THREAD_WAIT_WHILE( pt, !time_b_is_sync() );
 
-    	TMR_WAIT( pt, 1000 );
+        TMR_WAIT( pt, 1000 );
     }
 
 PT_END( pt );
