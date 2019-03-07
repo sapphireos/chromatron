4b4ef0640ca94301c2e61441abc84c1ab3097ea3
JeremyB <jeremybillheimer@gmail.com>
2019-03-06 20:43:17 -0600

    change clock polarity

--- src/hal/stm32h7/hal_i2s.c
+++ src/hal/stm32h7/hal_i2s.c
@@ -99,7 +99,7 @@ void hal_i2s_v_start( uint16_t sample_rate, uint8_t _sample_bits, bool _stereo )
 	i2s_handle.Init.DataFormat 			= I2S_DATAFORMAT_16B_EXTENDED;
 	i2s_handle.Init.MCLKOutput 			= I2S_MCLKOUTPUT_DISABLE;
 	i2s_handle.Init.AudioFreq 			= sample_rate;
-	i2s_handle.Init.CPOL 				= I2S_CPOL_HIGH;
+	i2s_handle.Init.CPOL 				= I2S_CPOL_LOW;
 	i2s_handle.Init.FirstBit 			= I2S_FIRSTBIT_MSB;
 	i2s_handle.Init.WSInversion 		= I2S_WS_INVERSION_DISABLE;
 	i2s_handle.Init.IOSwap 				= I2S_IO_SWAP_DISABLE;
