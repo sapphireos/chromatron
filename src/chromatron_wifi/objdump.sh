c659aa1689b78dfdc0a759003a5b8efe0dcc4259
JeremyB <jeremybillheimer@gmail.com>
2019-05-02 21:23:26 -0500

    pointer rename

--- src/chromatron_wifi/objdump.sh
+++ src/chromatron_wifi/objdump.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-./xtensa-lx106-elf-objdump -d -S .pioenvs/esp12e/firmware.elf
+# ./xtensa-lx106-elf-objdump -d -S .pioenvs/esp12e/firmware.elf
+./xtensa-lx106-elf-objdump-linux -d -S .pioenvs/esp12e/firmware.elf
