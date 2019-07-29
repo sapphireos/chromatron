5de88ad7d0193b4aa412927de2adfa84195ecc95
JeremyB <jeremybillheimer@gmail.com>
2019-07-29 09:01:28 -0500

    lib.py

---
+++ python/chromatron/chromatron/lib.py
@@ -0,0 +1,14 @@
+
+ARRAY_FUNCS = {		
+	'len': self._len,
+    'min': self._min,
+    'max': self._max,
+    'sum': self._sum,
+    'avg': self._avg,
+}
+
+
+def array_len(memory, addr, count):
+    pass
+
+
