a8245a97d76c0affbbb20faa75886ecab413cd20
JeremyB <jeremybillheimer@gmail.com>
2019-08-19 08:40:38 -0500

    debug

--- src/chromatron_wifi/objdump.sh
+++ src/chromatron_wifi/objdump.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# ./xtensa-lx106-elf-objdump -d -S .pioenvs/esp12e/firmware.elf
-./xtensa-lx106-elf-objdump-linux -d -S .pioenvs/esp12e/firmware.elf
+# ./xtensa-lx106-elf-objdump -d -S .pio/build/esp12e/firmware.elf
+./xtensa-lx106-elf-objdump-linux -d -S .pio/build/esp12e/firmware.elf
