7cc9abd0a903a60ed7162c93791b21fba3396733
JeremyB <jeremybillheimer@gmail.com>
2019-08-21 22:46:43 -0500

    VM running

--- src/chromatron_wifi/src/target.h
+++ src/chromatron_wifi/src/target.h
@@ -45,6 +45,6 @@
 #define VM_MAX_THREADS              4
 #define VM_MAX_IMAGE_SIZE   4096
 #define VM_MAX_CYCLES       32768
-#define VM_OPTIMIZED_DECODE
+// #define VM_OPTIMIZED_DECODE // this will crash the ESP
 
 #endif
