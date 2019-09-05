ff203f75f367c395e2e993688d1f09164abd8004
JeremyB <jeremybillheimer@gmail.com>
2019-09-05 09:07:50 -0500

    first try at influx sender

--- src/sapphireos/catbus.c
+++ src/sapphireos/catbus.c
@@ -28,7 +28,7 @@
 #include "hash.h"
 #include "catbus.h"
 #include "random.h"
-
+#include "influx.h"
 #include "timesync.h"
 
 
@@ -1224,6 +1224,8 @@ int8_t catbus_i8_publish( catbus_hash_t32 hash ){
     
         kv_v_notify_hash_set( hash );
     }
+
+    influx_v_transmit( hash );
 
     #ifdef ENABLE_TIME_SYNC
     ntp_ts_t ntp_timestamp = time_t_now();
---
+++ src/sapphireos/influx.h
@@ -0,0 +1,34 @@
+/*
+// <license>
+// 
+//     This file is part of the Sapphire Operating System.
+// 
+//     Copyright (C) 2013-2019  Jeremy Billheimer
+// 
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// </license>
+ */
+
+
+#ifndef INFLUX_H
+#define INFLUX_H
+
+#define INFLUX_DEFAULT_UDP_PORT 		8089
+
+void influx_v_init( void );
+void influx_v_transmit( catbus_hash_t32 hash );
+
+#endif
