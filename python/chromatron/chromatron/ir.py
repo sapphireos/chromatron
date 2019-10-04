40e6d38f0a0e5de43c26df9700cd24817abecf6f
JeremyB <jeremybillheimer@gmail.com>
2019-10-04 07:55:37 -0500

    undo some debug

--- python/chromatron/chromatron/ir.py
+++ python/chromatron/chromatron/ir.py
@@ -3220,8 +3220,6 @@ class Builder(object):
                 meta_names.append(var.name)
                 self.published_var_count += 1
 
-                print published_var,get_type_id(var.type), var.type, type(var), var
-
         # set up links
         packed_links = ''
         for link in self.links:
