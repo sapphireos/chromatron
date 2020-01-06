b55fe8d4884e8e9f138ae40ebac950ecfb8cbc3a
JeremyB <jeremybillheimer@gmail.com>
2020-01-05 18:23:14 -0600

    change time server port

--- src/sapphireos/timesync.h
+++ src/sapphireos/timesync.h
@@ -32,8 +32,8 @@
 
 #include "wifi_cmd.h"
 
-// #define TIME_SERVER_PORT                32037
-#define TIME_SERVER_PORT                32035
+#define TIME_SERVER_PORT                32037
+// #define TIME_SERVER_PORT                32035
 
 #define TIME_PROTOCOL_MAGIC             0x454d4954 // 'TIME' in ASCII
 #define TIME_PROTOCOL_VERSION           4
