add041dc861f45cabd391d8b040cc8167b3eebda
JeremyB <jeremybillheimer@gmail.com>
2020-02-09 12:36:22 -0600

    Revert "not sure it is possible to replicate the esp8266 build environment from scratch"
    This reverts commit 5032184747e1ea470543c5c27426b388f0b61dc5.

--- src/esp8266_test/user_main.c
+++ src/esp8266_test/user_main.c
@@ -70,17 +70,17 @@ void ICACHE_FLASH_ATTR user_init(void)
 {
     gpio_init();
 
-    // uart_init(115200, 115200);
-    // os_printf("SDK version:%s\n", system_get_sdk_version());
+    uart_init(115200, 115200);
+    os_printf("SDK version:%s\n", system_get_sdk_version());
 
     // Disable WiFi
-    	// wifi_set_opmode(NULL_MODE);
+    	wifi_set_opmode(NULL_MODE);
 
     PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
 
     os_timer_disarm(&ptimer);
     os_timer_setfn(&ptimer, (os_timer_func_t *)blinky, NULL);
-    os_timer_arm(&ptimer, 2000, 1);
+    os_timer_arm(&ptimer, 500, 1);
 
     // main();
 }
