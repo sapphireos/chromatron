10c5b1aff54eaa17cc3c5c92faa2e033ad2764c2
JeremyB <jeremybillheimer@gmail.com>
2020-04-02 22:09:14 -0500

    raw pixel driver in coproc

---
+++ src/coprocessor/pix_modes.h
@@ -0,0 +1,43 @@
+// <license>
+// 
+//     This file is part of the Sapphire Operating System.
+// 
+//     Copyright (C) 2013-2019  Jeremy Billheimer
+// 
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// </license>
+
+#ifndef _PIX_MODES_H_
+#define _PIX_MODES_H_
+
+
+#define PIX_MODE_OFF            0
+#define PIX_MODE_WS2801         1
+#define PIX_MODE_APA102         2
+#define PIX_MODE_WS2811         3
+#define PIX_MODE_PIXIE          4
+#define PIX_MODE_SK6812_RGBW    5
+#define PIX_MODE_ANALOG         128
+
+#define PIX_ORDER_RGB       0
+#define PIX_ORDER_RBG       1
+#define PIX_ORDER_GRB       2
+#define PIX_ORDER_BGR       3
+#define PIX_ORDER_BRG       4
+#define PIX_ORDER_GBR       5
+
+
+#endif
