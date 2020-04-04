c052dfe885c65d8fdbcf5b4a3f4fb15c92cfaad6
JeremyB <jeremybillheimer@gmail.com>
2020-04-04 15:15:12 -0500

    debug

--- src/hal/esp8266/user_main.c
+++ src/hal/esp8266/user_main.c
@@ -141,6 +141,8 @@ void ICACHE_FLASH_ATTR user_init(void)
 
     os_printf("\r\nESP8266 SDK version:%s\r\n", system_get_sdk_version());
 
+// return;
+
     register uint32_t *sp asm("a1");
     // os_printf("0x%08x\r\n", sp);
 
@@ -160,6 +162,7 @@ void ICACHE_FLASH_ATTR user_init(void)
   	wifi_set_opmode(NULL_MODE);
 
     PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
+// return;
 
     // os_timer_disarm(&ptimer);
     // os_timer_setfn(&ptimer, (os_timer_func_t *)blinky, NULL);
@@ -178,7 +181,7 @@ void ICACHE_FLASH_ATTR user_init(void)
             libs_v_init();
         }
     }
-
+// return;
 
     // start OS
     // on the ESP8266 this will return
