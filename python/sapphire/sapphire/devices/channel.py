e54b4fc40973f373d2db047da4bc472d3ed332af
JeremyB <jeremybillheimer@gmail.com>
2020-04-11 16:26:40 -0500

    clean up legacy channel stuff

--- python/sapphire/sapphire/devices/channel.py
+++ python/sapphire/sapphire/devices/channel.py
@@ -175,82 +175,6 @@ class UdpxChannel(Channel):
         self.sock.settimeout(timeout)
 
 
-class UdpxClientPoolChannel(Channel):
-
-    POOL_SIZE = 4
-
-    __q = Queue(maxsize=POOL_SIZE)
-
-    def __init__(self, host):
-        super(UdpxClientPoolChannel, self).__init__(host, 'network')
-
-        self.timeout = None
-        self.sock = None
-
-        self.q = self.__q
-
-    def open(self):
-        pass
-
-    def close(self):
-        if self.sock:
-            self.sock.shutdown()
-
-    def getSock(self):
-        # block on queue
-        self.q.put(None)
-
-        # create socket
-        self.sock = udpx.ClientSocket()
-
-        # set timeout
-        if self.timeout != None:
-            self.sock.settimeout(self.timeout)
-
-    def returnSock(self):
-        self.sock = None
-
-        # remove dummy Q item
-        self.q.get()
-
-    def read(self):
-        data = self.read_data
-        self.read_data = None
-
-        return data
-
-    def write(self, data, port=None):
-        self.getSock()
-
-        try:
-            #print "Sent %4d > %s" % (len(data), self.host)
-            self.sock.sendto(data, self.host)
-
-            self.read_data, host = self.sock.recvfrom()
-            #print "Recv %4d > %s" % (len(self.read_data), host)
-
-            # check host address
-            if host[0] != self.host[0]:
-                raise ChannelInvalidHostException(self.host)
-
-            else:
-                # set host so we have the host port
-                self.host = host
-
-        except socket.timeout:
-            raise ChannelTimeoutException(self.host)
-
-        except socket.error:
-            raise ChannelUnreachableException
-
-        finally:
-            self.returnSock()
-
-
-    def settimeout(self, timeout=None):
-        self.timeout = timeout
-
-
 UART_RX_BUF_SIZE = 255
 
 class SerialChannel(Channel):
@@ -436,184 +360,6 @@ class CmdUsartUDPHeader(StructField):
                   Uint16Field(_name="header_crc")]
 
         super(CmdUsartUDPHeader, self).__init__(_name="cmd_usart_udp_header", _fields=fields, **kwargs)
-
-
-class LegacySerialUDPChannel(Channel):
-    def __init__(self, host):
-        super(LegacySerialUDPChannel, self).__init__(host, 'serial_udp')
-
-        self.open(host)
-
-        # set up crc function
-        self.crc_func = crcmod.predefined.mkCrcFun('crc-aug-ccitt')
-
-        self.lport = CMD2_UDP_PORT
-        self.rport = CMD2_UDP_PORT
-
-    def open(self, host):
-        self.port = serial.Serial(host, baudrate=2000000)
-        self.settimeout(timeout=2.0)
-        self.host = self.port.port
-
-    def close(self):
-        self.port.close()
-
-    def _read_data(self, length):
-        start = time.time()
-        data = ''
-
-        while len(data) < length:
-            data += self.port.read(length)
-
-            if (len(data) < length) and ((time.time() - start) > self.port.timeout):
-                # raise ChannelTimeoutException()
-                return data
-
-        return data
-
-    def read(self):
-        try:
-            header = CmdUsartUDPHeaderLegacy()
-
-            # wait for SOF
-            sof = struct.unpack('<B', self._read_data(1))[0]
-
-            if sof != CMD_USART_UDP_SOF:
-                print("sof error", sof)
-                raise ChannelErrorException()
-
-            # wait for header data
-            header_data = self._read_data(header.size())
-
-            # unpack header
-            header.unpack(header_data)
-
-            header_crc = header.header_crc
-            header.header_crc = 0
-            computed_crc = self.crc_func(header.pack())
-
-            # check header
-            if header_crc != computed_crc:
-                print("read header error")
-                raise ChannelErrorException()
-
-            # receive data
-            data = self._read_data(header.data_len)
-            if len(data) != header.data_len:
-                print("invalid data length")
-                print(header.data_len, len(data))
-
-                raise ChannelErrorException()
-
-            # receive CRC
-            crc = struct.unpack('<H', self._read_data(2))[0]
-
-            # check crc
-            if self.crc_func(data) == crc:
-                return data
-
-            print("read crc error: %x %x" % (crc, self.crc_func(data)))
-            print(header.len, len(data))
-
-            raise ChannelErrorException()
-
-        except serial.SerialTimeoutException:
-            print("read timeout")
-            raise ChannelTimeoutException
-
-        except struct.error:
-            # print "read struct error"
-            raise ChannelErrorException
-
-
-    def _write_data(self, data):
-        while len(data) > 0:
-            count = len(data)
-
-            if count > UART_RX_BUF_SIZE:
-               count = UART_RX_BUF_SIZE
-
-            self.port.write(chr(count))
-            space = ord(self.port.read(1))
-
-            if space < count:
-                count = space
-
-            if count == 0:
-                time.sleep(0.002)
-                continue
-
-            self.port.write(data[:count])
-
-            # slice data
-            data = data[count:]
-
-    def write(self, data, port=None):
-        tries = 4
-
-        if port:
-            rport = port
-        else:
-            rport = self.rport
-
-        while tries > 0:
-
-            tries -= 1
-
-            try:
-                # flush serial input buffer
-                self.port.flushInput()
-
-                self._write_data(struct.pack('B', CMD_USART_UDP_SOF))
-
-                # set up header
-                header = CmdUsartUDPHeaderLegacy(
-                            version=CMD_USART_VERSION_LEGACY,
-                            lport=self.lport,
-                            rport=rport,
-                            data_len=len(data))
-
-                crc = self.crc_func(header.pack())
-                header.header_crc = crc
-
-                self._write_data(header.pack())
-
-                # send data
-                self._write_data(data)
-
-                # compute crc
-                crc = self.crc_func(data)
-
-                # send CRC
-                self._write_data(struct.pack('<H', crc))
-
-                # wait for ACK
-                response = struct.unpack('B', self.port.read(1))[0]
-
-                if response == CMD_USART_UDP_ACK:
-                    return
-
-                else:
-                    error = struct.unpack('B', self.port.read(1))[0]
-                    print("error: %d" % (error))
-
-                    time.sleep(0.5)
-
-            except Exception as e:
-                if tries == 0:
-                    raise
-
-                # try to reopen port
-                self.close()
-                self.open(self.host)
-
-
-        if tries == 0:
-            print("write tries exceeded")
-            raise ChannelErrorException()
-
-    def settimeout(self, timeout):
-        self.port.timeout = timeout
 
 
 class SerialUDPChannel(Channel):
@@ -820,33 +566,6 @@ class USBChannel(SerialChannel):
             raise ChannelInvalidHostException("Could not find attached device")
 
 
-class LegacyUSBUDPChannel(LegacySerialUDPChannel):
-    def __init__(self, host):
-        super(LegacyUSBUDPChannel, self).__init__(host)
-
-        self.open(host)
-
-    def close(self):
-        try:
-            super(LegacyUSBUDPChannel, self).close()
-
-        except AttributeError:
-            pass
-
-        except TypeError:
-            # we'll get a TypeError when __del__ is called
-            pass
-
-    def open(self, host):
-        try:
-            self.port = serial.Serial(host, baudrate=2000000)
-            self.settimeout(timeout=5.0)
-            self.host = self.port.port
-
-        except serial.serialutil.SerialException:
-            raise ChannelInvalidHostException("Could not find attached device")
-
-
 class USBUDPChannel(SerialUDPChannel):
     def __init__(self, host):
         super(USBUDPChannel, self).__init__(host)
@@ -939,16 +658,6 @@ def createChannel(host, port=None):
         ports = list(serial.tools.list_ports.comports())
 
         for port in ports:
-            # if port.vid == ATMEL_VID and port.pid == ATMEL_PID and port.manufacturer == MFG_NAME:
-            #     comport = port.device
-            #     porttype = 'legacy'
-            #     break
-
-            # elif port.vid == CHROMATRON_VID and port.pid == CHROMATRON_PID and port.manufacturer == MFG_NAME:
-            #     comport = port.device
-            #     porttype = 'udp_transport'
-            #     break
-
             if port.vid == CHROMATRON_VID and port.pid == CHROMATRON_PID:
                     comport = port.device
                     porttype = 'udp_transport'
@@ -971,12 +680,9 @@ def createChannel(host, port=None):
             except ChannelErrorException:
                 ch.close()
 
-                # fallback to legacy
-                # return LegacyUSBUDPChannel(comport)
-
     try:
         socket.inet_aton(host)
-        # return UdpxClientPoolChannel(host=(host, port))
+
         return UdpxChannel(host=(host, port))
 
     except:
@@ -988,13 +694,3 @@ if __name__ == '__main__':
 
     for port in ports:
         print(port.device, port.vid, port.pid, port.manufacturer, port.product)
-
-    # chan = createChannel('usb')
-    # print chan
-
-    # chan.write('meow')
-
-    # print chan.read()
-
-
-
