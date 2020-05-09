a50520802d47e46a743694b4446278c143d65b4b
JeremyB <jeremybillheimer@gmail.com>
2020-05-09 17:00:44 -0500

    more build updates

---
+++ build_esp8266_loader.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sapphiremake -p sapphire -p hal_esp8266 -p lib_coprocessor -p loader_esp8266 -c
+sapphiremake -p sapphire -p hal_esp8266 -p lib_coprocessor -p loader_esp8266 -t esp8266_loader --loader
