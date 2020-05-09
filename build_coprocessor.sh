e50c53a0e8bc79dbae57da27bc55398c5cc2dbad
JeremyB <jeremybillheimer@gmail.com>
2020-05-09 16:41:41 -0500

    more build updates

--- build_coprocessor.sh
+++ build_coprocessor.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 sapphiremake -p sapphire -p hal_xmega128a4u -p lib_coprocessor -p coprocessor -c
-sapphiremake -p sapphire -p hal_xmega128a4u -p lib_coprocessor -p coprocessor
+sapphiremake -p sapphire -p hal_xmega128a4u -p lib_coprocessor -p coprocessor -t coprocessor
 
 pushd src/hal/esp8266
 python bin_to_c_array.py
