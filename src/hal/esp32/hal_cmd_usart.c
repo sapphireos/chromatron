efabcfd2cc606023c244a2ddc3a25761793b2a79
JeremyB <jeremybillheimer@gmail.com>
2020-06-14 12:02:10 -0500

    cmd usart receive

--- src/hal/esp32/hal_cmd_usart.c
+++ src/hal/esp32/hal_cmd_usart.c
@@ -24,6 +24,7 @@
 
 #include "cpu.h"
 #include "system.h"
+#include "threading.h"
 
 #include "cmd_usart.h"
 
@@ -31,6 +32,53 @@
 
 #include "driver/uart.h"
 
+#define RX_FIFO_SIZE 256
+
+static uint8_t rx_fifo[RX_FIFO_SIZE];
+static uint16_t rx_ins;
+static uint16_t rx_ext;
+static uint16_t rx_size;
+
+static int16_t read_char( void ){
+
+    if( rx_size >= cnt_of_array(rx_fifo) ){
+
+        return -1;
+    }
+
+    uint8_t temp;
+    if( uart_read_bytes( HAL_CMD_UART, &temp, 1, 0 ) != 1 ){
+
+        return -2;
+    }
+
+    rx_fifo[rx_ins] = temp;
+    rx_ins++;
+    if( rx_ins >= cnt_of_array(rx_fifo) ){
+
+        rx_ins = 0;
+    }
+
+    rx_size++;
+
+    return temp;
+}
+
+PT_THREAD( uart_receiver_thread( pt_t *pt, void *state ) )
+{
+PT_BEGIN( pt );  
+    
+    while(1){
+        
+        THREAD_WAIT_WHILE( pt, read_char() < 0 );
+
+        while( read_char() >= 0 );
+
+        THREAD_YIELD( pt );
+    }
+
+PT_END( pt );
+}
 
 void cmd_usart_v_set_baud( baud_t baud ){
 
@@ -43,12 +91,12 @@ bool cmd_usart_b_received_char( void ){
 
 void cmd_usart_v_send_char( uint8_t data ){
 
-    uart_write_bytes(HAL_CMD_UART, (const char *) &data, 1);
+    uart_write_bytes( HAL_CMD_UART, (const char *) &data, 1 );
 }
 
 void cmd_usart_v_send_data( const uint8_t *data, uint16_t len ){
 
-    uart_write_bytes(HAL_CMD_UART, (const char *) data, len);
+    uart_write_bytes( HAL_CMD_UART, (const char *) data, len );
 }
 
 
@@ -59,19 +107,27 @@ int16_t cmd_usart_i16_get_char( void ){
         return -1;
     }
 
-    return -1;
+    uint8_t temp = rx_fifo[rx_ext];
+    rx_ext++;
+    if( rx_ext >= cnt_of_array(rx_fifo) ){
+
+        rx_ext = 0;
+    }
+    rx_size--;
+
+    return temp;
 }
 
 uint8_t cmd_usart_u8_get_data( uint8_t *data, uint8_t len ){
 
-    uint8_t rx_size = cmd_usart_u16_rx_size();
+    uint8_t size = cmd_usart_u16_rx_size();
 
-    if( len > rx_size ){
+    if( len > size ){
 
-        len = rx_size;
+        len = size;
     }
 
-    rx_size = len;
+    size = len;
 
     while( len > 0 ){
 
@@ -81,12 +137,12 @@ uint8_t cmd_usart_u8_get_data( uint8_t *data, uint8_t len ){
         len--;
     }
 
-    return rx_size;
+    return size;
 }
 
 uint16_t cmd_usart_u16_rx_size( void ){
 
-    return 0;
+    return rx_size;
 }
 
 void cmd_usart_v_flush( void ){
@@ -96,6 +152,11 @@ void cmd_usart_v_flush( void ){
 
 void hal_cmd_usart_v_init( void ){
 
+    thread_t_create( uart_receiver_thread,
+                     PSTR("cmd_uart_receiver"),
+                     0,
+                     0 );
+
     uart_config_t uart_config = {
         .baud_rate = 115200,
         .data_bits = UART_DATA_8_BITS,
@@ -103,9 +164,9 @@ void hal_cmd_usart_v_init( void ){
         .stop_bits = UART_STOP_BITS_1,
         .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
     };
-    uart_param_config(HAL_CMD_UART, &uart_config);
-    uart_set_pin(HAL_CMD_UART, HAL_CMD_TXD, HAL_CMD_RXD, -1, -1);
-    uart_driver_install(HAL_CMD_UART, HAL_CMD_RX_BUF_SIZE, 0, 0, NULL, 0);
+    uart_param_config( HAL_CMD_UART, &uart_config );
+    uart_set_pin( HAL_CMD_UART, HAL_CMD_TXD, HAL_CMD_RXD, -1, -1 );
+    uart_driver_install( HAL_CMD_UART, HAL_CMD_RX_BUF_SIZE, 0, 0, NULL, 0 );
 }
 
 
