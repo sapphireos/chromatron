1cf26ca000c641ad1ec4a7c6930b3b573749c2db
JeremyB <jeremybillheimer@gmail.com>
2020-10-18 10:16:59 -0500

    debug

--- src/lib_gfx/vm_cron.c
+++ src/lib_gfx/vm_cron.c
@@ -92,31 +92,6 @@ void vm_cron_v_init( void ){
              0 );
 }
 
-void vm_cron_v_load( uint8_t vm_id, vm_state_t *state, file_t f ){
-
-    // make sure this vm's cron jobs are unloaded first
-    vm_cron_v_unload( vm_id );
-
-    fs_v_seek( f, sizeof(uint32_t) + state->cron_start );
-
-    cron_job_t cron_job;
-    cron_job.vm_id = vm_id;
-
-    for( uint8_t i = 0; i < state->cron_count; i++ ){
-
-        fs_i16_read( f, (uint8_t *)&cron_job.cron, sizeof(cron_job.cron) );
-
-        list_node_t ln = list_ln_create_node2( &cron_job, sizeof(cron_job), MEM_TYPE_CRON_JOB );
-
-        if( ln < 0 ){
-
-            return;
-        }
-
-        list_v_insert_tail( &cron_list, ln );
-    }
-}
-
 void vm_cron_v_unload( uint8_t vm_id ){
 
     list_node_t ln = cron_list.head;
@@ -216,6 +191,34 @@ PT_END( pt );
 
 #endif
 
+void vm_cron_v_load( uint8_t vm_id, vm_state_t *state, file_t f ){
+
+    #ifdef ENABLE_TIME_SYNC
+
+    // make sure this vm's cron jobs are unloaded first
+    vm_cron_v_unload( vm_id );
+
+    fs_v_seek( f, sizeof(uint32_t) + state->cron_start );
+
+    cron_job_t cron_job;
+    cron_job.vm_id = vm_id;
+
+    for( uint8_t i = 0; i < state->cron_count; i++ ){
+
+        fs_i16_read( f, (uint8_t *)&cron_job.cron, sizeof(cron_job.cron) );
+
+        list_node_t ln = list_ln_create_node2( &cron_job, sizeof(cron_job), MEM_TYPE_CRON_JOB );
+
+        if( ln < 0 ){
+
+            return;
+        }
+
+        list_v_insert_tail( &cron_list, ln );
+    }
+
+    #endif
+}
 
 
 // this code is unfinished
