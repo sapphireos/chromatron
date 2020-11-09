5ef6686955825f7fdad90cc0d69bb5e341ddfafd
JeremyB <jeremybillheimer@gmail.com>
2020-11-09 09:12:59 -0600

    more docker timezone fixes

--- python/catbus/docker/timezone_service/run.sh
+++ python/catbus/docker/timezone_service/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo docker run -d --restart=always -v /etc/timezone:/etc/timezone:ro --network=host --name timezone_service sapphire_timezone_service:latest
+sudo docker run -d --restart=always -v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro --network=host --name timezone_service sapphire_timezone_service:latest
