0925c6b8cc8885fe02ecbeb0b242cc95653b1a1f
JeremyB <jeremybillheimer@gmail.com>
2020-12-14 16:55:36 -0600

    set VMs back to 4

--- src/hal/esp8266/target.h
+++ src/hal/esp8266/target.h
@@ -71,7 +71,7 @@
 #define VM_MAX_IMAGE_SIZE   	4096
 #define VM_MAX_CYCLES       	16000
 #define VM_MIN_DELAY                10
-#define VM_MAX_VMS                  1
+#define VM_MAX_VMS                  4
 #define VM_MAX_CALL_DEPTH           8
 #define VM_MAX_THREADS              8
 
