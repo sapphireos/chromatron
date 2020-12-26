79b97d631b82d4705fae41c58b7de43a9c6f21e4
JeremyB <jeremybillheimer@gmail.com>
2020-12-26 12:55:26 -0600

    cron replay

--- src/lib_gfx/vm_cron.c
+++ src/lib_gfx/vm_cron.c
@@ -181,6 +181,69 @@ PT_BEGIN( pt );
 PT_END( pt );
 }
 
+typedef struct{
+    uint8_t vm_id;
+    uint32_t cron_seconds;
+    datetime_t cron_time;
+} replay_state_t;
+
+PT_THREAD( cron_replay_thread( pt_t *pt, replay_state_t *state ) )
+{
+PT_BEGIN( pt ); 
+
+    // init clock to 24 hours ago
+    ntp_ts_t ntp_now = time_t_local_now();
+    state->cron_seconds = ntp_now.seconds - ( 24 * 60 * 60 );
+    datetime_v_seconds_to_datetime( state->cron_seconds, &state->cron_time );
+
+    
+    while(1){
+
+        ntp_ts_t ntp_local_now = time_t_local_now();
+
+        for( uint16_t i = 0; i < 600; i++ ){
+
+            datetime_v_increment_seconds( &state->cron_time );
+            state->cron_seconds++;
+
+            // run through job list
+            list_node_t ln = cron_list.head;
+            list_node_t next_ln;
+
+            while( ln > 0 ){
+
+                next_ln = list_ln_next( ln );
+
+                cron_job_t *entry = list_vp_get_data( ln );
+
+                if( entry->vm_id != state->vm_id ){
+
+                    goto next;
+                }
+
+                if( job_ready( &state->cron_time, entry ) ){
+
+                    int8_t status = vm_cron_i8_run_func( entry->vm_id, entry->cron.func_addr );                   
+                   
+                    log_v_debug_P( PSTR("Replaying cron job: %u for vm: %d status: %d"), entry->cron.func_addr, entry->vm_id, status );
+                }
+
+next:
+                ln = next_ln;
+            }   
+
+            if( state->cron_seconds >= ntp_local_now.seconds ){
+
+                THREAD_EXIT( pt );
+            }
+        }
+        
+        TMR_WAIT( pt, 20 );
+    }
+
+PT_END( pt );
+}
+
 #endif
 
 
@@ -223,6 +286,14 @@ void vm_cron_v_load( uint8_t vm_id, vm_state_t *state, file_t f ){
 
         list_v_insert_tail( &cron_list, ln );
     }
+
+    replay_state_t replay_state;
+    replay_state.vm_id = vm_id;
+
+    thread_t_create( THREAD_CAST(cron_replay_thread),
+             PSTR("cron_replay"),
+             &replay_state,
+             sizeof(replay_state) );
 
     #endif
 }
