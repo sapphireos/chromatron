1392a2c6c8444a9bcb718e81e95deacaacde04ff
JeremyB <jeremybillheimer@gmail.com>
2021-01-12 22:32:47 -0600

    debug

--- build_chromatron_esp32.sh
+++ build_chromatron_esp32.sh
@@ -3,5 +3,10 @@
 # sapphiremake -p sapphire -p hal_esp32 -p chromatron_no_led -c
 # sapphiremake -p sapphire -p hal_esp32 -p chromatron_no_led -t esp32
 
+
+sapphiremake -p sapphire -p hal_esp32 -p lib_battery -p lib_gfx -p lib_chromatron -p chromatron -c
+sapphiremake -p sapphire -p hal_esp32 -p lib_battery -p lib_gfx -p lib_chromatron -p chromatron -t esp32_single
+
 sapphiremake -p sapphire -p hal_esp32 -p lib_battery -p lib_gfx -p lib_chromatron -p chromatron -c
 sapphiremake -p sapphire -p hal_esp32 -p lib_battery -p lib_gfx -p lib_chromatron -p chromatron -t esp32
+
