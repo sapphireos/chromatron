757d230553f3b85849940a437b988a976c64e458
JeremyB <jeremybillheimer@gmail.com>
2021-02-11 23:26:40 -0600

    move fields tests

--- python/chromatron/test.sh
+++ python/chromatron/test.sh
@@ -3,4 +3,5 @@
 # PYTHONPATH=. pytest --cov=. --cov-report=html -s tests/test_services.py -v -x
 # PYTHONPATH=. pytest --cov=. --cov-report=html -s tests/test_link.py -v -x
 # PYTHONPATH=. pytest -s -x
-PYTHONPATH=. pytest --cov=. --cov-config=tests/.coveragerc --cov-report=html -s -x
+# PYTHONPATH=. pytest --cov=. --cov-config=tests/.coveragerc --cov-report=html -s -x
+PYTHONPATH=. pytest --cov=. --cov-config=tests/.coveragerc --cov-report=html tests/test_fields.py -s -x 
