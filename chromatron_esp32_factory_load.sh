26611247ca603ef45d8f06af50903019a4dec6d6
JeremyB <jeremybillheimer@gmail.com>
2021-05-06 19:43:40 -0500

    factory load script

---
+++ chromatron_esp32_factory_load.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+pushd src/bootloaders/loader_esp32
+./load_esp32_bootloader.sh
+popd
+sapphiremake -p chromatron --load_esp32
+
+echo "wait for boot..."
+sleep 20
+
+echo "set wifi"
+chromatron --host usb keys set wifi_ssid $CHROMATRON_WIFI_SSID
+chromatron --host usb keys set wifi_password $CHROMATRON_WIFI_PASS
+chromatron --host usb reboot
