8b4d340a9013a4f9186dd161409c8725fb05db10
JeremyB <jeremybillheimer@gmail.com>
2021-05-30 11:36:05 -0500

    fix an error message

--- python/chromatron/chromatron/code_gen.py
+++ python/chromatron/chromatron/code_gen.py
@@ -345,6 +345,9 @@ class cg1Call(cg1CodeNode):
             if self.target in THREAD_FUNCS:
                 raise SyntaxError("Parameter '%s' not found for function '%s'.  Thread functions require string parameters." % (e.var, self.target), self.lineno)
 
+            else:
+                raise
+
         return builder.call(self.target, params, lineno=self.lineno)
 
 class cg1If(cg1CodeNode):
