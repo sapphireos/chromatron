b87131757a337dce699ea6788dd00a3a4b79c7f6
JeremyB <jeremybillheimer@gmail.com>
2021-09-07 18:37:30 -0500

    pwm debug

--- src/hal/esp32/hal_pwm.c
+++ src/hal/esp32/hal_pwm.c
@@ -83,7 +83,7 @@ void pwm_v_init( void ){
 
 
     ledc_timer_config_t ledc_timer = {
-        .duty_resolution = LEDC_TIMER_13_BIT, // resolution of PWM duty
+        .duty_resolution = LEDC_TIMER_16_BIT, // resolution of PWM duty
         .freq_hz = 5000,                      // frequency of PWM signal
         .speed_mode = LEDC_HIGH_SPEED_MODE,           // timer mode
         .timer_num = LEDC_TIMER_0            // timer index
