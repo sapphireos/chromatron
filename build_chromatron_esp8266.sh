38103e77b1bd39b020c4c88fc41cef7c526d6987
JeremyB <jeremybillheimer@gmail.com>
2021-10-26 22:47:01 -0500

    update build script

--- build_chromatron_esp8266.sh
+++ build_chromatron_esp8266.sh
@@ -2,5 +2,5 @@
 
 # ./build_coprocessor.sh
 
-sapphiremake -p sapphire -p hal_esp8266 -p lib_coprocessor -p lib_battery -p lib_gfx -p lib_chromatron -p chromatron -c
-sapphiremake -p sapphire -p hal_esp8266 -p lib_coprocessor -p lib_battery -p lib_gfx -p lib_chromatron -p chromatron -t chromatron_classic
+sapphiremake -p sapphire -p hal_esp8266 -p lib_coprocessor -p lib_battery -p lib_veml7700 -p lib_gfx -p lib_chromatron -p chromatron -c
+sapphiremake -p sapphire -p hal_esp8266 -p lib_coprocessor -p lib_battery -p lib_veml7700 -p lib_gfx -p lib_chromatron -p chromatron -t chromatron_classic
