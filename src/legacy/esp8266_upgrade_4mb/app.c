356ae577806593bf99cfa17b903a73dba4e5e142
JeremyB <jeremybillheimer@gmail.com>
2022-02-03 08:37:54 -0600

    new upgrade projects

--- src/legacy/esp8266_upgrade_4mb/app.c
+++ src/legacy/esp8266_upgrade_4mb/app.c
@@ -24,22 +24,15 @@
 
 #include "app.h"
 
-#include "coprocessor.h"
-
 #ifndef ESP8266_UPGRADE
 #error "esp8266_upgrade requires ESP8266_UPGRADE defined in target.h!"
 #endif
-
+	
 
 PT_THREAD( app_thread( pt_t *pt, void *state ) )
 {       	
 PT_BEGIN( pt );  
-    
-    // log_v_debug_P( PSTR("coproc crc: 0x%04x"), coproc_u16_fw_crc() );
 
-    // char firmware_version[FW_VER_LEN];
-    // coproc_v_fw_version( firmware_version );
-    // log_v_debug_P( PSTR("coproc ver: %s"), firmware_version );
 
 PT_END( pt );	
 }
