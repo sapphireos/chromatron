02bebd4d1cea2d4b47b0e0ce7d8450a588ec10b8
JeremyB <jeremybillheimer@gmail.com>
2022-03-23 16:13:26 -0500

    sdk 4.4

--- src/hal/esp32/esp-idf/idf_as_lib/build/CMakeFiles/idf_as_lib.elf.dir/cmake_clean.cmake
+++ src/hal/esp32/esp-idf/idf_as_lib/build/CMakeFiles/idf_as_lib.elf.dir/cmake_clean.cmake
@@ -1,5 +1,12 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/idf_as_lib.elf.dir/main.c.o"
+  "bootloader/bootloader.bin"
+  "bootloader/bootloader.elf"
+  "bootloader/bootloader.map"
+  "config/sdkconfig.cmake"
+  "config/sdkconfig.h"
+  "flash_project_args"
+  "idf_as_lib.bin"
+  "CMakeFiles/idf_as_lib.elf.dir/main.c.obj"
   "idf_as_lib.elf"
   "idf_as_lib.elf.pdb"
 )
--- src/hal/esp32/esp-idf/idf_as_lib/build/cmake_install.cmake
+++ src/hal/esp32/esp-idf/idf_as_lib/build/cmake_install.cmake
@@ -27,21 +27,14 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
-  set(CMAKE_CROSSCOMPILING "FALSE")
+  set(CMAKE_CROSSCOMPILING "TRUE")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/jeremy/JEREMY/SAPPHIRE/chromatron/src/hal/esp-idf/examples/build_system/cmake/idf_as_lib/build/stubs/esp32/cmake_install.cmake")
-  include("/home/jeremy/JEREMY/SAPPHIRE/chromatron/src/hal/esp-idf/examples/build_system/cmake/idf_as_lib/build/stubs/freertos/cmake_install.cmake")
-  include("/home/jeremy/JEREMY/SAPPHIRE/chromatron/src/hal/esp-idf/examples/build_system/cmake/idf_as_lib/build/stubs/spi_flash/cmake_install.cmake")
+  include("/home/jeremy/JEREMY/SAPPHIRE/chromatron/src/hal/esp-idf/examples/build_system/cmake/idf_as_lib/build/esp-idf/cmake_install.cmake")
 
 endif()
 
