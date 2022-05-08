ab4edc6b5cc14cbc462baaf484614e5e9c8286fa
JeremyB <jeremybillheimer@gmail.com>
2022-05-08 14:15:19 -0500

    update script

--- chromatron_avr_factory_load.sh
+++ chromatron_avr_factory_load.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-avrdude -p atxmega128a4u -P usb -c jtag2pdi -U flash:w:src/coprocessor/main.hex
+avrdude -p atxmega128a4u -P usb -c jtag2pdi -U flash:w:src/legacy/coprocessor/main.hex
 
 
 # previous:
