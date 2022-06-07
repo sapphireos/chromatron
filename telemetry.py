e7c326f8303be887d158927984bf2ea91ab884fa
JeremyB <jeremybillheimer@gmail.com>
2022-06-07 13:38:04 -0500

    telemetry parser stuff

--- telemetry.py
+++ telemetry.py
@@ -1,4 +1,5 @@
 
+from pprint import pprint
 from elysianfields import *
 
 
@@ -21,18 +22,55 @@ class TelemetryLog0(StructField):
 
 
 with open('telemetry_log', 'rb') as f:
-	data = f.read()
+    data = f.read()
 
 
 chunk_len = TelemetryLog0().size()
 
+SPIRES = [
+    # 0,
+    101469591321096,
+    259794970753544,
+    48693033187848,
+    272989110286856,
+    53091079698952,
+    198242321695656,    
+]
+
+NAMES = {
+    198242321695656: 'spire1',
+    53091079698952: 'spire2',
+    272989110286856: 'spire3',
+    101469591321096: 'spire4',
+    48693033187848: 'spire5',
+    259794970753544: 'spire6',
+}
+
+nodes = {}
+
 while len(data) > 0:
-	chunk = data[:chunk_len]
+    chunk = data[:chunk_len]
+    data = data[chunk_len:]
 
-	log_info = TelemetryLog0().unpack(chunk)
+    log_info = TelemetryLog0().unpack(chunk)
 
-	print(log_info)
+    # if log_info.src_addr not in SPIRES:
+    #     continue
 
-	data = data[chunk_len:]
+    # # print(log_info.src_addr)
+    # # print(log_info.src_addr in NAMES)
+    # if log_info.src_addr in NAMES:
+    #     # print('meow')
+    #     log_info.src_addr = NAMES[log_info.src_addr]
+
+    nodes[log_info.src_addr] = log_info
+
+    # print(log_info)
+for k, v in nodes.items():
+    # print(hex(k))
+    # print(v)
+    print(v)
+
+    
 
 
