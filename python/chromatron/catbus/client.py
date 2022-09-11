cb17abcb23c50defbd388f18d61daecfe91b677c
JeremyB <jeremybillheimer@gmail.com>
2022-09-11 15:46:10 -0500

    log local port on mqtt bridge clients

--- python/chromatron/catbus/client.py
+++ python/chromatron/catbus/client.py
@@ -71,6 +71,10 @@ class BaseClient(object):
     def __str__(self):
         return f'BaseClient({self._connected_host})'
 
+    @property
+    def local_port(self):
+        return self._sock.getsockname()[1]
+
     def _exchange(self, msg, host=None, timeout=0.5, tries=16):
         msg.header.universe = self.universe
 
