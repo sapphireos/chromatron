ad9b5d3b90d4f41215283a8197e8af259cbdb07f
JeremyB <jeremybillheimer@gmail.com>
2022-09-18 17:05:03 -0500

    eeprom driver

--- src/hal/atmega128rfa1/hal_eeprom.c
+++ src/hal/atmega128rfa1/hal_eeprom.c
@@ -38,18 +38,65 @@ void ee_v_init( void ){
 
 bool ee_b_busy( void ){
 
-    return !eeprom_is_ready();
+    return ( EECR & ( 1 << EEPE ) ) != 0;
 }
 
 // write a byte to eeprom and wait for the completion of the write
 void ee_v_write_byte_blocking( uint16_t address, uint8_t data ){
+\
+    // read original byte
+    uint8_t original = ee_u8_read_byte( address );
 
-    eeprom_write_byte( (uint8_t *)address, data );
+    // check if data is not changing
+    if( data == original ){
+        
+        return;
+    }
+
+    #ifdef __SIM__
+    
+    array[address] = data;
+    
+    #else
+    
+    // busy wait
+    while( ee_b_busy() );
+
+    ATOMIC; // must be atomic, an interrupt here will cause the write to fail
+    
+    EEAR = address; // set address
+    EEDR = data; // set byte to be written
+    
+    // clear control bits, this sets mode to erase and write
+    EECR &= ~( ( 1 << EEPM0 ) | ( 1 << EEPM1 ) );
+
+    // check if erasing
+    if( data == 0xff ){
+        
+        // set for erase only
+        EECR |= ( 1 << EEPM0 );
+    }
+
+    EECR |= ( 1 << EEMPE ); // set master programming enable
+    EECR |= ( 1 << EEPE ); // set programming enable
+    
+    END_ATOMIC;
+    
+    #endif
 }
 
 void ee_v_write_block( uint16_t address, const uint8_t *data, uint16_t len ){
-
-    eeprom_write_block( (const void *)data, (void *)address, len );
+    
+    while( len > 0 ){
+        
+        ee_v_write_byte_blocking( address, *data );
+        
+        data++;
+        len--;
+        address++;
+        
+        sys_v_wdt_reset();
+    }
 }
 
 void ee_v_erase_block( uint16_t address, uint16_t len ){
@@ -67,12 +114,56 @@ void ee_v_erase_block( uint16_t address, uint16_t len ){
 
 uint8_t ee_u8_read_byte( uint16_t address ){
 
-    return eeprom_read_byte( (uint8_t *)address );
+    #ifdef __SIM__
+    
+    return array[address];
+    
+    #else
+    
+    // busy wait
+    while( ee_b_busy() );
+
+    ATOMIC;
+    
+    EEAR = address; // set address
+    
+    EECR |= ( 1 << EERE ); // set read enable
+    
+    END_ATOMIC;
+    
+    return EEDR; // return byte
+    
+    #endif
 }
 
 void ee_v_read_block( uint16_t address, uint8_t *data, uint16_t len ){
+    
+    // busy wait
+    while( ee_b_busy() );
 
-    eeprom_read_block( (void *)data, (const void *)address, len );
+    for( uint16_t i = 0; i < len; i++ ){
+        
+        #ifdef __SIM__
+    
+        *data = array[address];
+        
+        #else
+
+        ATOMIC;
+        
+        EEAR = address; // set address
+        
+        EECR |= ( 1 << EERE ); // set read enable
+        
+        END_ATOMIC;
+        
+        *data = EEDR; // return byte
+        
+        #endif
+
+        data++;
+        address++;
+    }
 }
 
 
