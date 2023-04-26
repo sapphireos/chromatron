ce6fc06673a4db939fd929500662c4979f80d2e7
JeremyB <jeremybillheimer@gmail.com>
2023-04-25 21:53:28 -0500

    add more light level filtering

--- src/lib_battery/light_sensor.c
+++ src/lib_battery/light_sensor.c
@@ -27,14 +27,72 @@
 
 #include "veml7700.h"
 
+// 1 minute average
+static uint32_t filtered_light;
+static int32_t current_delta;
+
+KV_SECTION_META kv_meta_t light_sensor_kv[] = {
+    {CATBUS_TYPE_UINT32,     0, KV_FLAGS_READ_ONLY, &filtered_light,	0, "light_level"},   
+    {CATBUS_TYPE_INT32,      0, KV_FLAGS_READ_ONLY, &current_delta,		0, "light_delta"},   
+};
+
+PT_THREAD( light_sensor_thread( pt_t *pt, void *state ) );
+
+
 void light_sensor_v_init( void ){
 
 	// veml7700 is initialized separately!
 	
 	// veml7700_v_init();
+
+    thread_t_create( light_sensor_thread,
+                 PSTR("light_sensor"),
+                 0,
+                 0 );
 }
 
 uint32_t light_sensor_u32_read( void ){
 
-	return veml7700_u32_read_als();
+	return filtered_light;
 }
+
+
+
+PT_THREAD( light_sensor_thread( pt_t *pt, void *state ) )
+{       	
+PT_BEGIN( pt );
+
+	static uint8_t counter;
+	counter = 0;
+
+	static uint32_t accum;
+	accum = 0;
+	
+	while(1){
+
+		TMR_WAIT( pt, 1000 );
+
+		uint32_t light = veml7700_u32_read_als();
+
+		accum += light;
+
+
+		counter++;
+
+		if( counter >= 60 ){
+
+			counter = 0;
+
+			int32_t temp = accum / 60;
+
+			current_delta = temp - (int32_t)filtered_light;
+
+			filtered_light = temp;
+
+			accum = 0;
+		}
+	}
+
+PT_END( pt );	
+}
+
