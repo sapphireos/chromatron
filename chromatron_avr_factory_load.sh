514fe20f3e8adfc9e43b451159971c5a24b8fa46
JeremyB <jeremybillheimer@gmail.com>
2023-09-24 15:56:27 -0500

    restore factory load script defaults

--- chromatron_avr_factory_load.sh
+++ chromatron_avr_factory_load.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# avrdude -p atxmega128a4u -P usb -c jtag2pdi -U boot:w:src/bootloaders/loader_xmega128a4u/loader.hex -U flash:w:src/legacy/coprocessor/main.hex -U fuse2:w:0xBE:m -U fuse5:w:0xE4:m
-avrdude -p atxmega128a4u -P usb -c jtag2pdi -U flash:w:src/legacy/coprocessor/main.hex
+avrdude -p atxmega128a4u -P usb -c jtag2pdi -U boot:w:src/bootloaders/loader_xmega128a4u/loader.hex -U flash:w:src/legacy/coprocessor/main.hex -U fuse2:w:0xBE:m -U fuse5:w:0xE4:m
+# avrdude -p atxmega128a4u -P usb -c jtag2pdi -U flash:w:src/legacy/coprocessor/main.hex
 
 
 # previous:
