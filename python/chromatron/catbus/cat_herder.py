49d282f75a6834fc63886a47082a695f1414cf34
JeremyB <jeremybillheimer@gmail.com>
2023-10-29 20:02:03 -0500

    improvements to cat herder

--- python/chromatron/catbus/cat_herder.py
+++ python/chromatron/catbus/cat_herder.py
@@ -29,89 +29,254 @@
 
 import time
 import threading
-from copy import deepcopy
+from copy import copy, deepcopy
 import logging
 
 from catbus.directory import Directory
 from catbus.client import Client, BackgroundClient
+from catbus import query_tags
 
+class WorkQueueEmpty(Exception):
+	pass
 
-
-class CatHerder(threading.Thread):
-	def __init__(self):
+class Worker(threading.Thread):
+	def __init__(self, herder=None):
 		super().__init__()
 
-		self.max_workers = 4
-		self._workers = {}
+		self._herder = herder
 
-		self._directory = Directory()
-		
-		self._request_keys = {'wifi_rssi'}
-
-		self._lock = threading.Lock()
 		self._stop_event = threading.Event()
-		self._semaphore = threading.Semaphore(self.max_workers)
+
+		self.client = Client()
 
 		self.daemon = True
 		self.start()
+
+	def stop(self):
+		logging.info(f'Stopping worker')
+		self._stop_event.set()
+
+	def run(self):
+		logging.info(f'Starting worker')
+
+		while not self._stop_event.is_set():
+			self._herder.wait_for_timer()
+
+			request_keys = self._herder.request_keys
+
+			kv_data = {}
+
+			try:
+				while True:
+					host, host_key = self._herder.get_work_item()
+				
+					self.client.connect(host)
+					kv = self.client.get_keys(*request_keys)
+
+					kv_data[host_key] = kv
+
+			except WorkQueueEmpty:
+				pass
+
+			self._herder.update_kv(kv_data)
+
+		logging.info(f'Worker exit')
+
+
+class CatHerder(threading.Thread):
+	def __init__(self, workers=1, rate=1.0):
+		super().__init__()
+
+		self._max_workers = workers
+		self._rate = rate
+
+		self._query = []
+		self._request_keys = []
+		self._kv_data = {}
+
+		self._directory = Directory()
+		
+		self._lock = threading.Lock()
+		self._stop_event = threading.Event()
+		self._timer_event = threading.Event()
+		
+		self._work_q = []
+		self._workers = []
+
+		self.daemon = True
+		self.start()
+
+	def stop(self):
+		self._stop_event.set()
+
+	def join(self):
+		for worker in self._workers:
+			worker.join()
+
+		super().join()
+
+	@property
+	def query(self):
+		with self._lock:
+			return copy(self._query)
+
+	@query.setter
+	def query(self, value):
+		with self._lock:
+			self._query = copy(value)
+
+	@property
+	def request_keys(self):
+		with self._lock:
+			return copy(self._request_keys)
+
+	@request_keys.setter
+	def request_keys(self, value):
+		with self._lock:
+			self._request_keys = copy(value)	
+
+	@property
+	def data(self):
+		with self._lock:
+			return copy(self._kv_data)
+
+	def update_kv(self, kv_data={}):
+		directory = self.directory
+
+		for k, v in kv_data.items():
+			v.update(directory[k])
+
+		with self._lock:
+			self._kv_data.update(kv_data)
+
+	def get_work_item(self):
+		with self._lock:
+			try:
+				return self._work_q.pop(0)
+
+			except IndexError:
+				raise WorkQueueEmpty
+
+	def wait_for_timer(self):
+		self._timer_event.wait()
 
 	@property
 	def directory(self):
 		return self._directory.get_directory()
 
-	@property
-	def data(self):
-		data = {}
-
-		with self._lock:	
-			for host, worker in self._workers.items():
-				data[host] = worker.data
-
-		return data
-
 	def run(self):
+		logging.info(f'Starting Herder')
+
+		self._workers = [Worker(self) for i in range(self._max_workers)]
+
 		while not self._stop_event.is_set():
-			time.sleep(1.0)
-		
-			for host_str, device in self.directory.items():
-				host = device['host']
+			time.sleep(self._rate)
 
-				if host_str in self._workers:
-					continue
+			work_q = []
 
-				self._workers[host_str] = BackgroundClient(host, semaphore=self._semaphore)
-				logging.info(f'Starting worker: {host}')
-
-
-			for worker in self._workers.values():
-				worker.set_keys({'kv_test_key': 123})
-
-			# prune
-			remove = []
-			for host_str, cat in self._workers.items():
-				if host_str not in self.directory:
-					remove.append(host_str)
-
-			for host_str in remove:
-				logging.info(f'Pruning worker: {host_str}')
-				self._workers[host_str].stop()
-				del self._workers[host_str]
-
-
-			# update data
 			with self._lock:
-				self._kv_data = {}
+				for host_str, device in self.directory.items():
+					if not query_tags(self._query, device['query']):
+						continue
+
+					host = device['host']
+
+					work_q.append((host, host_str))
+
+					# print(host, device['name'], device['query'], self._query)
+
+				self._work_q = work_q
+
+			self._timer_event.set()
+			self._timer_event.clear()
 
 
-
-		for worker in self._workers.values():
+		for worker in self._workers:
 			worker.stop()
 
-		for worker in self._workers.values():
-			worker.join()
+		# set timer event so workers will stop
+		self._timer_event.set()
 
-	def stop(self):
-		self._stop_event.set()
+		logging.info(f'Herder exit')
+
+
+
+
+# class CatHerder(threading.Thread):
+# 	def __init__(self):
+# 		super().__init__()
+
+# 		self.max_workers = 4
+# 		self._workers = {}
+
+# 		self._directory = Directory()
+		
+# 		self._request_keys = {'wifi_rssi'}
+
+# 		self._lock = threading.Lock()
+# 		self._stop_event = threading.Event()
+# 		self._semaphore = threading.Semaphore(self.max_workers)
+
+# 		self.daemon = True
+# 		self.start()
+
+# 	@property
+# 	def directory(self):
+# 		return self._directory.get_directory()
+
+# 	@property
+# 	def data(self):
+# 		data = {}
+
+# 		with self._lock:	
+# 			for host, worker in self._workers.items():
+# 				data[host] = worker.data
+
+# 		return data
+
+# 	def run(self):
+# 		while not self._stop_event.is_set():
+# 			time.sleep(1.0)
+		
+# 			for host_str, device in self.directory.items():
+# 				host = device['host']
+
+# 				if host_str in self._workers:
+# 					continue
+
+# 				self._workers[host_str] = BackgroundClient(host, semaphore=self._semaphore)
+# 				logging.info(f'Starting worker: {host}')
+
+
+# 			for worker in self._workers.values():
+# 				worker.set_keys({'kv_test_key': 123})
+
+# 			# prune
+# 			remove = []
+# 			for host_str, cat in self._workers.items():
+# 				if host_str not in self.directory:
+# 					remove.append(host_str)
+
+# 			for host_str in remove:
+# 				logging.info(f'Pruning worker: {host_str}')
+# 				self._workers[host_str].stop()
+# 				del self._workers[host_str]
+
+
+# 			# update data
+# 			with self._lock:
+# 				self._kv_data = {}
+
+
+
+# 		for worker in self._workers.values():
+# 			worker.stop()
+
+# 		for worker in self._workers.values():
+# 			worker.join()
+
+# 	def stop(self):
+# 		self._stop_event.set()
 
 
 if __name__ == "__main__":
@@ -120,10 +285,10 @@ if __name__ == "__main__":
 
 	from pprint import pprint
 
-
 	c = CatHerder()
 
-	# c = BackgroundClient(('10.0.0.211', 44632))
+	c.query = ['living_room']
+	c.request_keys = ['wifi_rssi', 'gfx_max_dimmer', 'gfx_sub_dimmer']
 
 
 	while True:
@@ -131,8 +296,6 @@ if __name__ == "__main__":
 			time.sleep(1.0)
 
 			pprint(c.data)
-
-			# c.set_keys({'kv_test_key': 123})
 
 		except KeyboardInterrupt:
 			break
