7d1a45c7b3e53abf16a4a255c3d3e18c867cc67f
JeremyB <jeremybillheimer@gmail.com>
2023-12-23 12:34:28 -0600

    reduce max handles on esp8266

--- src/hal/esp8266/target.h
+++ src/hal/esp8266/target.h
@@ -93,7 +93,7 @@
 // #define ENABLE_LIST_ATOMIC
 
 // memory
-#define MAX_MEM_HANDLES         255
+#define MAX_MEM_HANDLES         192
 #define MEM_MAX_STACK           4096
 #define MEM_HEAP_SIZE			16384
 
