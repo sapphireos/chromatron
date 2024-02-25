da7e2a0b80ca7a8a55a151c7aa9f7f80115f7c1e
JeremyB <jeremybillheimer@gmail.com>
2024-02-25 16:39:48 -0600

    ffs fw erase thread fix

--- src/sapphireos/ffs_fw.c
+++ src/sapphireos/ffs_fw.c
@@ -486,7 +486,7 @@ void ffs_fw_v_erase( uint8_t partition, bool immediate ){
         return;
     }
 
-    if( sys_u8_get_mode() != SYS_MODE_SAFE ){
+    if( sys_u8_get_mode() == SYS_MODE_SAFE ){
         
         immediate = TRUE;
     }
