7a33420b0c06b8d8518aa7a7e892a1f6c6ff7162
JeremyB <jeremybillheimer@gmail.com>
2024-12-14 12:17:16 -0600

    debug

--- src/hal/esp32/user_main.c
+++ src/hal/esp32/user_main.c
@@ -77,8 +77,8 @@ void sapphire_main()
     
     // set run time logging
     #ifdef ENABLE_TRACE
-    esp_log_level_set("*", ESP_LOG_INFO);
-    // esp_log_level_set("*", ESP_LOG_VERBOSE);
+    // esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("*", ESP_LOG_VERBOSE);
     // esp_log_level_set("*", ESP_LOG_DEBUG);
     #endif
 
