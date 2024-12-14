06db464809609f4fa301f031da8a6e7aa30025d9
JeremyB <jeremybillheimer@gmail.com>
2024-12-13 21:19:02 -0600

    wifi esp32 reset stored router on unexpected disconnection to force a scan

--- src/hal/esp32/hal_wifi.c
+++ src/hal/esp32/hal_wifi.c
@@ -254,6 +254,47 @@ void hal_wifi_v_init( void ){
     // instead of safe mode we could just do a restart (within the recovery counter).
     // if we hit the recovery counter we go to safe mode either way.
     
+    
+
+
+typedef enum {
+    WIFI_REASON_UNSPECIFIED              = 1,
+    WIFI_REASON_AUTH_EXPIRE              = 2,
+    WIFI_REASON_AUTH_LEAVE               = 3,
+    WIFI_REASON_ASSOC_EXPIRE             = 4,
+    WIFI_REASON_ASSOC_TOOMANY            = 5,
+    WIFI_REASON_NOT_AUTHED               = 6,
+    WIFI_REASON_NOT_ASSOCED              = 7,
+    WIFI_REASON_ASSOC_LEAVE              = 8,
+    WIFI_REASON_ASSOC_NOT_AUTHED         = 9,
+    WIFI_REASON_DISASSOC_PWRCAP_BAD      = 10,
+    WIFI_REASON_DISASSOC_SUPCHAN_BAD     = 11,
+    WIFI_REASON_BSS_TRANSITION_DISASSOC  = 12,
+    WIFI_REASON_IE_INVALID               = 13,
+    WIFI_REASON_MIC_FAILURE              = 14,
+    WIFI_REASON_4WAY_HANDSHAKE_TIMEOUT   = 15,
+    WIFI_REASON_GROUP_KEY_UPDATE_TIMEOUT = 16,
+    WIFI_REASON_IE_IN_4WAY_DIFFERS       = 17,
+    WIFI_REASON_GROUP_CIPHER_INVALID     = 18,
+    WIFI_REASON_PAIRWISE_CIPHER_INVALID  = 19,
+    WIFI_REASON_AKMP_INVALID             = 20,
+    WIFI_REASON_UNSUPP_RSN_IE_VERSION    = 21,
+    WIFI_REASON_INVALID_RSN_IE_CAP       = 22,
+    WIFI_REASON_802_1X_AUTH_FAILED       = 23,
+    WIFI_REASON_CIPHER_SUITE_REJECTED    = 24,
+
+    WIFI_REASON_INVALID_PMKID            = 53,
+
+    WIFI_REASON_BEACON_TIMEOUT           = 200,
+    WIFI_REASON_NO_AP_FOUND              = 201,
+    WIFI_REASON_AUTH_FAIL                = 202,
+    WIFI_REASON_ASSOC_FAIL               = 203,
+    WIFI_REASON_HANDSHAKE_TIMEOUT        = 204,
+    WIFI_REASON_CONNECTION_FAIL          = 205,
+    WIFI_REASON_AP_TSF_RESET             = 206,
+    WIFI_REASON_ROAMING                  = 207,
+} wifi_err_reason_t;
+
 
     */
 
@@ -856,6 +897,7 @@ static void scan_cb( void ){
     for( uint32_t i = 0; i < ap_count; i++ ){
 
         // trace_printf( "%s %u %d", ap_info[i].ssid, ap_info[i].primary, ap_info[i].rssi );
+        log_v_debug_P( PSTR("%s %u %d"), ap_info[i].ssid, ap_info[i].primary, ap_info[i].rssi );
 
         int8_t router = -1;
 
@@ -1344,6 +1386,23 @@ end:
     THREAD_WAIT_WHILE( pt, wifi_b_connected() && !wifi_shutdown );
     
     log_v_debug_P( PSTR("Wifi disconnected: %d Last RSSI: %d ch: %d"), disconnect_reason, wifi_rssi, wifi_channel );
+
+    // if we are not shutting down, 
+    if( !wifi_shutdown && !sys_b_is_shutting_down() ){
+
+        // assume this is an unintentional disconnection
+        // we will reset the stored router so 
+        // we do a scan on the next attempt
+
+        // reset router
+        wifi_router = -1;
+        wifi_channel = -1;
+        memset( wifi_bssid, 0, sizeof(wifi_bssid) );
+
+        kv_i8_persist( __KV__wifi_channel );
+        kv_i8_persist( __KV__wifi_bssid );
+        kv_i8_persist( __KV__wifi_router );
+    }
 
     wifi_v_reset_scan_timeout();
 
