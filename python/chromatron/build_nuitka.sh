fc1da978600ca1cea2bcddb6791cda17f0a9d1b1
JeremyB <jeremybillheimer@gmail.com>
2024-12-28 10:21:42 -0600

    update build script

--- python/chromatron/build_nuitka.sh
+++ python/chromatron/build_nuitka.sh
@@ -15,25 +15,25 @@ python3 -m nuitka --output-dir=bin/chromatron --standalone --onefile --verbose -
 cp $(pwd)/bin/chromatron/chromatron.bin ~/.local/bin/chromatron
 
 # # build catbus tool
-python3 -m nuitka --output-dir=bin/catbus --include-package=pygments --nofollow-import-to=PyQT5 --nofollow-import-to=numpy --standalone --onefile --verbose --verbose-output=nuitka.log catbus.py
+python3 -m nuitka --output-dir=bin/catbus --standalone --onefile --verbose --verbose-output=nuitka.log catbus.py
 # # install
 # ln -s $(pwd)/bin/catbus/catbus.dist/catbus.bin ~/.local/bin/catbus
 cp $(pwd)/bin/catbus/catbus.bin ~/.local/bin/catbus
 
 # build fx tool
-python3 -m nuitka --output-dir=bin/fx --include-package=pygments --nofollow-import-to=PyQT5 --nofollow-import-to=numpy --standalone --onefile --verbose --verbose-output=nuitka.log fx.py
+python3 -m nuitka --output-dir=bin/fx --standalone --onefile --verbose --verbose-output=nuitka.log fx.py
 # # install
 # ln -s $(pwd)/bin/fx/fx.dist/fx.bin ~/.local/bin/fx
 cp $(pwd)/bin/fx/fx.bin ~/.local/bin/fx
 
 # # build sapphiremake tool
-python3 -m nuitka --output-dir=bin/sapphiremake --include-package=pygments --nofollow-import-to=PyQT5 --nofollow-import-to=numpy --include-package-data=sapphire.buildtools --standalone --onefile --verbose --verbose-output=nuitka.log sapphiremake.py
+python3 -m nuitka --output-dir=bin/sapphiremake --include-package-data=sapphire.buildtools --standalone --onefile --verbose --verbose-output=nuitka.log sapphiremake.py
 # # install
 # ln -s $(pwd)/bin/sapphiremake/sapphiremake.dist/sapphiremake.bin ~/.local/bin/sapphiremake
 cp $(pwd)/bin/sapphiremake/sapphiremake.bin ~/.local/bin/sapphiremake
 
 # # build sapphireconsole tool
-python3 -m nuitka --output-dir=bin/sapphireconsole --include-package=pygments --nofollow-import-to=PyQT5 --nofollow-import-to=numpy --include-package-data=sapphire.buildtools --standalone --onefile --verbose --verbose-output=nuitka.log sapphireconsole.py
+python3 -m nuitka --output-dir=bin/sapphireconsole --standalone --onefile --verbose --verbose-output=nuitka.log sapphireconsole.py
 # # install
 # ln -s $(pwd)/bin/sapphireconsole/sapphireconsole.dist/sapphireconsole.bin ~/.local/bin/sapphireconsole
 cp $(pwd)/bin/sapphireconsole/sapphireconsole.bin ~/.local/bin/sapphireconsole
