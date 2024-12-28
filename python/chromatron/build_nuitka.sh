2e5867f438abaf7db9b8d40dd95ceb438c495c2a
JeremyB <jeremybillheimer@gmail.com>
2024-12-28 10:04:24 -0600

    more nuitka tweaks

--- python/chromatron/build_nuitka.sh
+++ python/chromatron/build_nuitka.sh
@@ -9,12 +9,26 @@
 
 
 # build chromatron tool
-# python3 -m nuitka --include-package=pygments --nofollow-import-to=PyQT5 --nofollow-import-to=numpy --standalone --onefile --verbose --verbose-output=nuitka.log chromatron.py
+python3 -m nuitka --output-dir=bin/chromatron --include-package=pygments --nofollow-import-to=PyQT5 --nofollow-import-to=numpy --standalone --onefile --verbose --verbose-output=nuitka.log chromatron.py
 # install
-# ln -s $(pwd)/chromatron.dist/chromatron.bin ~/.local/bin/chromatron
+ln -s $(pwd)/bin/chromatron/chromatron.dist/chromatron.bin ~/.local/bin/chromatron
 
+# # build catbus tool
+# python3 -m nuitka --output-dir=bin/catbus --include-package=pygments --nofollow-import-to=PyQT5 --nofollow-import-to=numpy --standalone --onefile --verbose --verbose-output=nuitka.log catbus.py
+# # install
+# ln -s $(pwd)/bin/catbus/catbus.dist/catbus.bin ~/.local/bin/catbus
 
-# build sapphiremake tool
-python3 -m nuitka --include-package=pygments --nofollow-import-to=PyQT5 --nofollow-import-to=numpy --standalone --onefile --verbose --verbose-output=nuitka.log sapphiremake.py
-# install
-# ln -s $(pwd)/chromatron.dist/chromatron.bin ~/.local/bin/chromatron
+# # build fx tool
+# python3 -m nuitka --output-dir=bin/fx --include-package=pygments --nofollow-import-to=PyQT5 --nofollow-import-to=numpy --standalone --onefile --verbose --verbose-output=nuitka.log fx.py
+# # install
+# ln -s $(pwd)/bin/fx/fx.dist/fx.bin ~/.local/bin/fx
+
+# # build sapphiremake tool
+# python3 -m nuitka --output-dir=bin/sapphiremake --include-package=pygments --nofollow-import-to=PyQT5 --nofollow-import-to=numpy --include-package-data=sapphire.buildtools --standalone --onefile --verbose --verbose-output=nuitka.log sapphiremake.py
+# # install
+# ln -s $(pwd)/bin/sapphiremake/sapphiremake.dist/sapphiremake.bin ~/.local/bin/sapphiremake
+
+# # build sapphireconsole tool
+# python3 -m nuitka --output-dir=bin/sapphireconsole --include-package=pygments --nofollow-import-to=PyQT5 --nofollow-import-to=numpy --include-package-data=sapphire.buildtools --standalone --onefile --verbose --verbose-output=nuitka.log sapphireconsole.py
+# # install
+# ln -s $(pwd)/bin/sapphireconsole/sapphireconsole.dist/sapphireconsole.bin ~/.local/bin/sapphireconsole
---
+++ python/chromatron/catbus.py
@@ -0,0 +1,3 @@
+from catbus import main
+if __name__ == '__main__':
+    main()
