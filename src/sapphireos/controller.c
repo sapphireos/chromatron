de3abe80df547583f2219209cbf4458fba478f8c
JeremyB <jeremybillheimer@gmail.com>
2025-02-13 17:13:46 -0600

    add extra stuff to controller status

--- src/sapphireos/controller.c
+++ src/sapphireos/controller.c
@@ -24,12 +24,16 @@
 
 #include "keyvalue.h"
 #include "logging.h"
+#include "memory.h"
 #include "sapphire.h"
 #include "config.h"
 
 #include "controller.h"
 
 #include "link.h"
+#include "system.h"
+#include "threading.h"
+#include "wifi.h"
 
 /*
 
@@ -558,11 +562,31 @@ static void send_announce( void ){
 
 static void init_status_msg( controller_msg_status_t *msg ){
 
-	msg->gfx_sync_group = 0;
-
-	kv_i8_get( __KV__gfx_sync_group_hash, &msg->gfx_sync_group, sizeof(msg->gfx_sync_group) );
+	memset( msg, 0, sizeof(controller_msg_status_t) );
 
 	catbus_v_get_query( &msg->query );
+
+	msg->ip 			= cfg_ip_get_ipaddr();
+	msg->uptime 		= tmr_u64_get_system_time_us();
+	msg->mode 			= sys_u8_get_mode();
+	msg->rssi 			= wifi_i8_rssi();
+	msg->wifi_channel   = wifi_i8_get_channel();
+	msg->cpu_percent    = thread_u8_get_cpu_percent();
+	msg->used_heap      = mem2_u16_get_used();
+
+	kv_i8_get( __KV__pixel_power, &msg->pixel_power, sizeof(msg->pixel_power) );
+	kv_i8_get( __KV__gfx_sync_group_hash, &msg->gfx_sync_group, sizeof(msg->gfx_sync_group) );
+	kv_i8_get( __KV__gfx_master_dimmer, &msg->gfx_master_dimmer, sizeof(msg->gfx_master_dimmer) );
+	kv_i8_get( __KV__gfx_sub_dimmer, &msg->gfx_sub_dimmer, sizeof(msg->gfx_sub_dimmer) );
+	kv_i8_get( __KV__vm_status, &msg->vm_status_0, sizeof(msg->vm_status_0) );
+	kv_i8_get( __KV__vm_status_1, &msg->vm_status_1, sizeof(msg->vm_status_1) );
+	kv_i8_get( __KV__vm_status_2, &msg->vm_status_2, sizeof(msg->vm_status_2) );
+	kv_i8_get( __KV__vm_status_3, &msg->vm_status_3, sizeof(msg->vm_status_3) );
+
+	kv_i8_get( __KV__batt_volts, &msg->batt_volts, sizeof(msg->batt_volts) );
+	kv_i8_get( __KV__batt_charge_current, &msg->batt_charge_current, sizeof(msg->batt_charge_current) );
+	kv_i8_get( __KV__batt_temp, &msg->batt_temp, sizeof(msg->batt_temp) );
+	kv_i8_get( __KV__light_level, &msg->light_level, sizeof(msg->light_level) );
 }
 
 static void send_status( void ){
@@ -752,8 +776,15 @@ static void process_announce( controller_msg_announce_t *msg, sock_addr_t *raddr
 	}
 }
 
+void controller_v_on_received_status( controller_msg_status_t *msg, sock_addr_t *raddr ){
+
+}
 
 static void process_status( controller_msg_status_t *msg, sock_addr_t *raddr ){
+
+	// run callback
+	// hmmmmmmmm
+	controller_v_on_received_status( msg, raddr );
 
 	if( controller_state != STATE_LEADER ){
 
