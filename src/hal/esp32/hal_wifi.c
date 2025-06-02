150388f2d380c3c4086e652d607cd96f913a17bd
JeremyB <jeremybillheimer@gmail.com>
2025-06-02 15:02:59 -0500

    Init AP netif on ESP32, fixes broken soft AP mode

--- src/hal/esp32/hal_wifi.c
+++ src/hal/esp32/hal_wifi.c
@@ -319,6 +319,7 @@ typedef enum {
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_sta();
+    esp_netif_create_default_wifi_ap();
     
     esp_read_mac( wifi_mac, ESP_MAC_WIFI_STA );
     
