c75ab03119018d727d3f8bbbbf3d91e320e8323e
JeremyB <jeremybillheimer@gmail.com>
2026-01-14 23:04:42 -0600

    turn off igmp and mcast

--- src/sapphireos/wifi.c
+++ src/sapphireos/wifi.c
@@ -32,11 +32,11 @@
 
 #if defined(ENABLE_WIFI) && !defined(AVR)
 
-static ip_addr4_t igmp_groups[WIFI_MAX_IGMP];
+// static ip_addr4_t igmp_groups[WIFI_MAX_IGMP];
 
 
 PT_THREAD( wifi_echo_thread( pt_t *pt, void *state ) );
-PT_THREAD( igmp_thread( pt_t *pt, void *state ) );
+// PT_THREAD( igmp_thread( pt_t *pt, void *state ) );
 
 void wifi_v_init( void ){
 
@@ -47,10 +47,10 @@ void wifi_v_init( void ){
     //     return;
     // }
 
-    thread_t_create( igmp_thread,
-                     PSTR("igmp"),
-                     0,
-                     0 );
+    // thread_t_create( igmp_thread,
+    //                  PSTR("igmp"),
+    //                  0,
+    //                  0 );
     
     thread_t_create( 
                 wifi_echo_thread,
@@ -82,114 +82,114 @@ PT_END( pt );
 }
 
 
-int8_t wifi_i8_igmp_join( ip_addr4_t mcast_ip ){
+// int8_t wifi_i8_igmp_join( ip_addr4_t mcast_ip ){
 
-    // if( sys_u8_get_mode() == SYS_MODE_SAFE ){
+//     // if( sys_u8_get_mode() == SYS_MODE_SAFE ){
 
-    //     return 0;
-    // }
+//     //     return 0;
+//     // }
 
-    for( uint8_t i = 0; i < cnt_of_array(igmp_groups); i++ ){
+//     for( uint8_t i = 0; i < cnt_of_array(igmp_groups); i++ ){
 
-        if( ip_b_addr_compare( mcast_ip, igmp_groups[i] ) ){
+//         if( ip_b_addr_compare( mcast_ip, igmp_groups[i] ) ){
 
-            // already joined
-            return 0;
-        }
-    }
+//             // already joined
+//             return 0;
+//         }
+//     }
 
-    // add new group
-    for( uint8_t i = 0; i < cnt_of_array(igmp_groups); i++ ){
+//     // add new group
+//     for( uint8_t i = 0; i < cnt_of_array(igmp_groups); i++ ){
 
-        if( ip_b_is_zeroes( igmp_groups[i] ) ){
+//         if( ip_b_is_zeroes( igmp_groups[i] ) ){
 
-            igmp_groups[i] = mcast_ip;
+//             igmp_groups[i] = mcast_ip;
 
-            // check if wifi is connected, if so, join here
-            // if not, we'll join in the igmp_thread.
-            if( wifi_b_connected() ){
+//             // check if wifi is connected, if so, join here
+//             // if not, we'll join in the igmp_thread.
+//             if( wifi_b_connected() ){
 
-                hal_wifi_i8_igmp_join( igmp_groups[i] );   
-            }
+//                 hal_wifi_i8_igmp_join( igmp_groups[i] );   
+//             }
 
-            break;
-        }
-    }
+//             break;
+//         }
+//     }
     
-    return 0;
-}
+//     return 0;
+// }
 
-int8_t wifi_i8_igmp_leave( ip_addr4_t mcast_ip ){
+// int8_t wifi_i8_igmp_leave( ip_addr4_t mcast_ip ){
 
-    // if( sys_u8_get_mode() == SYS_MODE_SAFE ){
+//     // if( sys_u8_get_mode() == SYS_MODE_SAFE ){
 
-    //     return 0;
-    // }
+//     //     return 0;
+//     // }
 
-    for( uint8_t i = 0; i < cnt_of_array(igmp_groups); i++ ){
+//     for( uint8_t i = 0; i < cnt_of_array(igmp_groups); i++ ){
 
-        if( ip_b_addr_compare( mcast_ip, igmp_groups[i] ) ){
+//         if( ip_b_addr_compare( mcast_ip, igmp_groups[i] ) ){
 
-            hal_wifi_i8_igmp_leave( mcast_ip );
+//             hal_wifi_i8_igmp_leave( mcast_ip );
 
-            igmp_groups[i] = ip_a_addr(0,0,0,0);
+//             igmp_groups[i] = ip_a_addr(0,0,0,0);
 
-            break;
-        }
-    }
+//             break;
+//         }
+//     }
     
-    return 0;
-}
+//     return 0;
+// }
 
 
-PT_THREAD( igmp_thread( pt_t *pt, void *state ) )
-{
-PT_BEGIN( pt );
+// PT_THREAD( igmp_thread( pt_t *pt, void *state ) )
+// {
+// PT_BEGIN( pt );
     
-    while(1){
+//     while(1){
 
-        THREAD_WAIT_WHILE( pt, !wifi_b_connected() );
+//         THREAD_WAIT_WHILE( pt, !wifi_b_connected() );
 
-        TMR_WAIT( pt, 1000 );
+//         TMR_WAIT( pt, 1000 );
 
-        for( uint8_t i = 0; i < cnt_of_array(igmp_groups); i++ ){
+//         for( uint8_t i = 0; i < cnt_of_array(igmp_groups); i++ ){
 
-            if( ip_b_is_zeroes( igmp_groups[i] ) ){
+//             if( ip_b_is_zeroes( igmp_groups[i] ) ){
 
-                continue;
-            }
+//                 continue;
+//             }
 
-            int8_t status = hal_wifi_i8_igmp_join( igmp_groups[i] );
+//             int8_t status = hal_wifi_i8_igmp_join( igmp_groups[i] );
 
-            if( status != 0 ){
+//             if( status != 0 ){
 
-                log_v_error_P( PSTR("IGMP join failed: %d group: %d.%d.%d.%d"), status, igmp_groups[i].ip3, igmp_groups[i].ip2, igmp_groups[i].ip1, igmp_groups[i].ip0 );
-            }
-        }
+//                 log_v_error_P( PSTR("IGMP join failed: %d group: %d.%d.%d.%d"), status, igmp_groups[i].ip3, igmp_groups[i].ip2, igmp_groups[i].ip1, igmp_groups[i].ip0 );
+//             }
+//         }
 
-        THREAD_WAIT_WHILE( pt, wifi_b_connected() && !sys_b_is_shutting_down() );
+//         THREAD_WAIT_WHILE( pt, wifi_b_connected() && !sys_b_is_shutting_down() );
 
-        // if shutting down, or lost connection, leave all groups so we'll rejoin later
-        if( !wifi_b_connected() || sys_b_is_shutting_down() ){
+//         // if shutting down, or lost connection, leave all groups so we'll rejoin later
+//         if( !wifi_b_connected() || sys_b_is_shutting_down() ){
 
-            for( uint8_t i = 0; i < cnt_of_array(igmp_groups); i++ ){
+//             for( uint8_t i = 0; i < cnt_of_array(igmp_groups); i++ ){
 
-                if( ip_b_is_zeroes( igmp_groups[i] ) ){
+//                 if( ip_b_is_zeroes( igmp_groups[i] ) ){
 
-                    continue;
-                }
+//                     continue;
+//                 }
 
-                hal_wifi_i8_igmp_leave( igmp_groups[i] );
-            }
-        }
+//                 hal_wifi_i8_igmp_leave( igmp_groups[i] );
+//             }
+//         }
 
-        if( sys_b_is_shutting_down() ){
+//         if( sys_b_is_shutting_down() ){
 
-            THREAD_EXIT( pt );
-        }
-    }
+//             THREAD_EXIT( pt );
+//         }
+//     }
 
-PT_END( pt );
-}
+// PT_END( pt );
+// }
 
 #endif
