ab99ffdaaf2f379665dab7ce049a4993e22990d3
JeremyB <jeremybillheimer@gmail.com>
2026-01-21 01:00:04 -0600

    work on link4

---
+++ src/sapphireos/link4.c
@@ -0,0 +1,27 @@
+
+#include "system.h"
+#include "timers.h"
+#include "sockets.h"
+#include "threading.h"
+#include "fs.h"
+#include "keyvalue.h"
+#include "catbus.h"
+#include "list.h"
+#include "hash.h"
+#include "util.h"
+#include "config.h"
+#include "random.h"
+#include "device_db.h"
+
+#include "link4.h"
+
+// #define NO_LOGGING
+#include "logging.h"
+
+
+
+
+void link4_v_init( void ){
+
+}
+
---
+++ src/sapphireos/link4.h
@@ -0,0 +1,8 @@
+#ifndef __LINK4_H__
+#define __LINK4_H__
+
+
+void link4_v_init( void );
+
+
+#endif
