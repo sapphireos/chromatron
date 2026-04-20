f4166f0b9ce385d7ec47913497fcfab6f33fd265
JeremyB <jeremybillheimer@gmail.com>
2026-04-20 10:03:49 -0500

    link4 debug

--- src/sapphireos/link4.c
+++ src/sapphireos/link4.c
@@ -437,7 +437,7 @@ PT_BEGIN( pt );
             	if( changed ){
 
             		// force timer so we transmit now
-            		link_state->transmit_timeout 	   = 1;
+            		link_state->transmit_timeout = 1;
 					link_state->transmit_timer = 0;
             	}
 
@@ -580,70 +580,75 @@ PT_BEGIN( pt );
                     goto next;
             	}
             	// not timed out, check database
-            	else if( link_state->database_h > 0 ){
+            	else if( link_state->database_h <= 0 ){
 
-            		link4_data_t *database = (link4_data_t *)mem2_vp_get_ptr( link_state->database_h );
+                    // no database present, we are done processing
+                    goto next;
+                }
 
-            		for( int i = 0; i < database_count( link_state->database_h ); i++ ){
+                goto next;
 
-            			if( database->timeout > 0 ){
+        		link4_data_t *database = (link4_data_t *)mem2_vp_get_ptr( link_state->database_h );
 
-            				database->timeout--;
-            			}
+        		for( int i = 0; i < database_count( link_state->database_h ); i++ ){
 
-            			if( database->timeout > 0 ){
+        			if( database->timeout > 0 ){
 
-                            continue;
-                        }
-
-        				log_v_info_P( PSTR("Data timed out: %d.%d.%d.%d hash: 0x%08x"),
-                            database->ip.ip3,
-                            database->ip.ip2,
-                            database->ip.ip1,
-                            database->ip.ip0,
-                            link_state->link.dest_key
-                        );
-
-        				uint16_t old_database_size = mem2_u16_get_size( link_state->database_h );
-        				uint16_t new_database_size = old_database_size - sizeof(link4_data_t);
-
-        				if( new_database_size == 0 ){
-
-        					// easy path, just release db
-        					mem2_v_free( link_state->database_h );
-        					link_state->database_h = -1;
-        				}
-        				else{
-
-            				mem_handle_t new_database_h = mem2_h_alloc( new_database_size );
-
-            				if( new_database_h <= 0 ){
-
-			        			log_v_error_P( PSTR("alloc fail") );
-
-			                	goto next;
-			        		}
-
-							link4_data_t *new_database = (link4_data_t *)mem2_vp_get_ptr( new_database_h );
-
-							// copy old data items into new db, skipping this current item we are deleting
-			        		for( int j = 0; j < database_count( link_state->database_h ); j++ ){
-
-			        			if( j == i ){
-
-			        				continue;
-			        			}
-
-			        			*new_database = database[j];
-			        			new_database++;
-			        		}
-
-			        		// release old db, set new on
-			        		mem2_v_free( link_state->database_h );
-			        		link_state->database_h = new_database_h;
-			        	}
+        				database->timeout--;
         			}
-            	}
+
+        			if( database->timeout > 0 ){
+
+                        continue;
+                    }
+
+    				log_v_info_P( PSTR("Data timed out: %d.%d.%d.%d hash: 0x%08x"),
+                        database->ip.ip3,
+                        database->ip.ip2,
+                        database->ip.ip1,
+                        database->ip.ip0,
+                        link_state->link.dest_key
+                    );
+
+    				uint16_t old_database_size = mem2_u16_get_size( link_state->database_h );
+    				uint16_t new_database_size = old_database_size - sizeof(link4_data_t);
+
+    				if( new_database_size == 0 ){
+
+    					// easy path, just release db
+    					mem2_v_free( link_state->database_h );
+    					link_state->database_h = -1;
+    				}
+    				else{
+
+        				mem_handle_t new_database_h = mem2_h_alloc2( new_database_size, MEM_TYPE_LINK4_DB );
+
+        				if( new_database_h <= 0 ){
+
+		        			log_v_error_P( PSTR("alloc fail") );
+
+		                	goto next;
+		        		}
+
+						link4_data_t *new_database = (link4_data_t *)mem2_vp_get_ptr( new_database_h );
+
+						// copy old data items into new db, skipping this current item we are deleting
+		        		for( int j = 0; j < database_count( link_state->database_h ); j++ ){
+
+		        			if( j == i ){
+
+		        				continue;
+		        			}
+
+		        			*new_database = database[j];
+		        			new_database++;
+		        		}
+
+		        		// release old db, set new on
+		        		mem2_v_free( link_state->database_h );
+		        		link_state->database_h = new_database_h;
+		        	}
+    			}
             }
             
 next:
@@ -781,7 +786,7 @@ PT_BEGIN( pt );
         	if( link_state->database_h <= 0 ){
 
         		// create database
-	            link_state->database_h = mem2_h_alloc( sizeof(link4_data_t) );
+	            link_state->database_h = mem2_h_alloc2( sizeof(link4_data_t), MEM_TYPE_LINK4_DB );
 
 	            if( link_state->database_h < 0 ){
 	            		
@@ -826,7 +831,7 @@ PT_BEGIN( pt );
         		uint16_t old_database_size = mem2_u16_get_size( link_state->database_h );
 
         		// no match, create item
-        		mem_handle_t new_database_h = mem2_h_alloc( sizeof(link4_data_t) + old_database_size );
+        		mem_handle_t new_database_h = mem2_h_alloc2( sizeof(link4_data_t) + old_database_size, MEM_TYPE_LINK4_DB );
 
         		if( new_database_h <= 0 ){
 
