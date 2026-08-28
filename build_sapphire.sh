84c0d3529988799942635841497578a2a1059bc3
JeremyB <jeremybillheimer@gmail.com>
2026-08-28 10:14:17 -0500

    build script for blue sapphire

---
+++ build_sapphire.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sapphiremake -p hal_atmega128rfa1 -p sapphire -p lib_battery -p lib_veml7700 -p lib_ssd1306 -p lib_mpu9250 -p lib_amg8833 -p lib_hd44780 -p lib_pca9685 -p lib_hx711 -p lib_rfm95w -p lib_gfx -p lib_chromatron -p chromatron -t sapphire -c
+sapphiremake -p hal_atmega128rfa1 -p sapphire -p lib_battery -p lib_veml7700 -p lib_ssd1306 -p lib_mpu9250 -p lib_amg8833 -p lib_hd44780 -p lib_pca9685 -p lib_hx711 -p lib_rfm95w -p lib_gfx -p lib_chromatron -p chromatron -t sapphire
